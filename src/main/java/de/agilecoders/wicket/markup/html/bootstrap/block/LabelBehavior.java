02de4253d5319333b039f2402001cb0f6f5ec270
Michael Haitz <michael.haitz@gmx.com>
2012-07-27 21:07:51 +0200

    add Label component

---
+++ src/main/java/de/agilecoders/wicket/markup/html/bootstrap/block/LabelBehavior.java
@@ -0,0 +1,43 @@
+package de.agilecoders.wicket.markup.html.bootstrap.block;
+
+import de.agilecoders.wicket.markup.html.bootstrap.behavior.BootstrapBaseBehavior;
+import de.agilecoders.wicket.markup.html.bootstrap.behavior.CssClassNameAppender;
+import de.agilecoders.wicket.util.Components;
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.ComponentTag;
+
+/**
+ * TODO: document
+ *
+ * @author miha
+ * @version 1.0
+ */
+public class LabelBehavior extends BootstrapBaseBehavior {
+    private LabelType type = LabelType.Default;
+
+    public LabelType type() {
+        return type;
+    }
+
+    public void type(LabelType type) {
+        this.type = type;
+    }
+
+    protected String className() {
+        return "label";
+    }
+
+    @Override
+    public void onComponentTag(Component component, ComponentTag tag) {
+        super.onComponentTag(component, tag);
+
+        Components.assertTag(component, tag, "span");
+    }
+
+    @Override
+    public void onConfigure(Component component) {
+        super.onConfigure(component);
+
+        component.add(new CssClassNameAppender(className(), type().cssClassName(className())));
+    }
+}
---
+++ src/main/java/de/agilecoders/wicket/markup/html/bootstrap/block/LabelType.java
@@ -0,0 +1,32 @@
+package de.agilecoders.wicket.markup.html.bootstrap.block;
+
+import de.agilecoders.wicket.markup.html.bootstrap.behavior.CssClassNameAppender;
+import de.agilecoders.wicket.markup.html.bootstrap.behavior.CssClassNameProvider;
+
+/**
+ * TODO: document
+ *
+ * @author miha
+ * @version 1.0
+ */
+public enum LabelType implements CssClassNameProvider {
+    Default, Success, Warning, Important, Info, Inverse;
+
+    @Override
+    public String cssClassName() {
+        return equals(Default) ? "" : name().toLowerCase();
+    }
+
+    public String cssClassName(String prefix) {
+        return equals(Default) ? "" : prefix + "-" + name().toLowerCase();
+    }
+
+    @Override
+    public CssClassNameAppender newCssClassNameAppender() {
+        return new CssClassNameAppender(cssClassName());
+    }
+
+    public CssClassNameAppender newCssClassNameAppender(String prefix) {
+        return new CssClassNameAppender(cssClassName(prefix));
+    }
+}
