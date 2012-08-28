ae45182320a73b8a5f64f2ab123941acf93e1378
Michael Haitz <michael.haitz@gmx.com>
2012-08-28 13:15:59 +0200

    remove dependecy between ActiveThemeProvider and ThemeProvider

--- src/main/java/de/agilecoders/wicket/settings/SessionThemeProvider.java
+++ src/main/java/de/agilecoders/wicket/settings/SessionThemeProvider.java
@@ -1,24 +1,29 @@
 package de.agilecoders.wicket.settings;
 
 import com.google.common.base.Strings;
+import de.agilecoders.wicket.Bootstrap;
+import org.apache.wicket.Application;
 import org.apache.wicket.Session;
+import org.apache.wicket.WicketRuntimeException;
 
 /**
- * TODO: document
+ * An {@link ActiveThemeProvider} implementation that stores the active theme
+ * in the user session as style.
  *
  * @author miha
  * @version 1.0
  */
 public class SessionThemeProvider implements ActiveThemeProvider {
 
-    private ThemeProvider themeProvider;
-
-    public SessionThemeProvider(ThemeProvider themeProvider) {
-        this.themeProvider = themeProvider;
-    }
-
-    void setThemeProvider(ThemeProvider themeProvider) {
-        this.themeProvider = themeProvider;
+    /**
+     * @return the {@link ThemeProvider} implementation
+     */
+    private ThemeProvider themeProvider() {
+        if (Application.exists()) {
+            return Bootstrap.getSettings(Application.get()).getThemeProvider();
+        } else {
+            throw new WicketRuntimeException("no application assigned to current thread");
+        }
     }
 
     @Override
@@ -26,15 +31,15 @@ public class SessionThemeProvider implements ActiveThemeProvider {
         String style = Session.get().getStyle();
 
         if (Strings.isNullOrEmpty(style)) {
-            return themeProvider.defaultTheme();
+            return themeProvider().defaultTheme();
         } else {
-            return themeProvider.byName(style);
+            return themeProvider().byName(style);
         }
     }
 
     @Override
     public void setActiveTheme(String themeName) {
-        setActiveTheme(themeProvider.byName(themeName));
+        setActiveTheme(themeProvider().byName(themeName));
     }
 
     @Override
@@ -48,10 +53,13 @@ public class SessionThemeProvider implements ActiveThemeProvider {
         }
     }
 
+    /**
+     * checks on existing session, if there isn't one it will be created.
+     */
     private void assertBoundSession() {
         Session session = Session.get();
 
-        if (Strings.isNullOrEmpty(session.getId())) {
+        if (session.isTemporary()) {
             session.bind();
         }
     }
