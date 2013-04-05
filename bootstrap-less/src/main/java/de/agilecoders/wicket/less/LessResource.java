322d351fbeaea557b22a002c4f6f55deff2d0e3f
Ben Day <ben__day@hotmail.co.uk>
2013-04-05 09:39:28 +0100

    Fixing missed merges

--- bootstrap-less/src/main/java/de/agilecoders/wicket/less/LessResource.java
+++ bootstrap-less/src/main/java/de/agilecoders/wicket/less/LessResource.java
@@ -1,17 +1,5 @@
 package de.agilecoders.wicket.less;
 
-<<<<<<< HEAD
-=======
-import de.agilecoders.wicket.BootstrapLess;
-import org.apache.wicket.Application;
->>>>>>> 7b7904945bf891c04f5053f26f7f2b1f0976e735
-import org.apache.wicket.WicketRuntimeException;
-import org.apache.wicket.util.io.Connections;
-import org.apache.wicket.util.io.IOUtils;
-import org.apache.wicket.util.time.Time;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -20,6 +8,14 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
+
+import org.apache.wicket.Application;
+import org.apache.wicket.WicketRuntimeException;
+import org.apache.wicket.util.io.Connections;
+import org.apache.wicket.util.io.IOUtils;
+import org.apache.wicket.util.time.Time;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Represents a less resource.
