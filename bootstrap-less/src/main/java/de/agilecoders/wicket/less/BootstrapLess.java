d1bd2ed2dc29e7066788943c43e6a99090ed542c
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-04-09 15:50:16 +0300

    Issue #161 - Remove old classes and add a demo application uses for unit tests

--- bootstrap-less/src/main/java/de/agilecoders/wicket/less/BootstrapLess.java
+++ bootstrap-less/src/main/java/de/agilecoders/wicket/less/BootstrapLess.java
@@ -1,8 +1,6 @@
 package de.agilecoders.wicket.less;
 
 import org.apache.wicket.Application;
-import org.apache.wicket.MetaDataKey;
-import org.apache.wicket.core.util.resource.locator.caching.CachingResourceStreamLocator;
 import org.apache.wicket.markup.html.IPackageResourceGuard;
 import org.apache.wicket.markup.html.SecurePackageResourceGuard;
 
@@ -10,12 +8,6 @@ import org.apache.wicket.markup.html.SecurePackageResourceGuard;
  * Bootstrap less compiler settings accessor class
  */
 public final class BootstrapLess {
-
-    /**
-     * The {@link org.apache.wicket.MetaDataKey} used to retrieve the {@link IBootstrapLessCompilerSettings} from the Wicket {@link Appendable}.
-     */
-    private static final MetaDataKey<IBootstrapLessCompilerSettings> BOOTSTRAP_LESS_SETTINGS_METADATA_KEY = new MetaDataKey<IBootstrapLessCompilerSettings>() {
-    };
 
     /**
      * Construct.
@@ -28,14 +20,8 @@ public final class BootstrapLess {
      * Installs given settings for given application
      *
      * @param app      The current application
-     * @param settings The settings to use
      */
-    public static void install(final Application app, final IBootstrapLessCompilerSettings settings) {
-        app.setMetaData(BOOTSTRAP_LESS_SETTINGS_METADATA_KEY, settings);
-
-        if (settings.useLessCompiler()) {
-            app.getResourceSettings().setResourceStreamLocator(new CachingResourceStreamLocator(new LessResourceStreamLocator()));
-        }
+    public static void install(final Application app) {
 
         LessCacheManager cacheManager = new LessCacheManager();
         cacheManager.install(app);
@@ -45,28 +31,5 @@ public final class BootstrapLess {
             SecurePackageResourceGuard securePackageResourceGuard = (SecurePackageResourceGuard) resourceGuard;
             securePackageResourceGuard.addPattern("+*.less");
         }
-    }
-
-    /**
-     * returns the {@link IBootstrapLessCompilerSettings} which are assigned to given application
-     *
-     * @param app The current application
-     * @return assigned {@link IBootstrapLessCompilerSettings}
-     */
-    public static IBootstrapLessCompilerSettings getSettings(final Application app) {
-        return app.getMetaData(BOOTSTRAP_LESS_SETTINGS_METADATA_KEY);
-    }
-
-    /**
-     * returns the {@link IBootstrapLessCompilerSettings} which are assigned to current application
-     *
-     * @return assigned {@link IBootstrapLessCompilerSettings}
-     */
-    public static IBootstrapLessCompilerSettings getSettings() {
-        if (Application.exists()) {
-            return getSettings(Application.get());
-        }
-
-        throw new IllegalStateException("there is no active application assigned to this thread.");
     }
 }
