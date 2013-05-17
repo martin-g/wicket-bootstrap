f1f88e755d3d4919bb90ec98da7acf6345afd51b
Michael Haitz <michael.haitz@gmx.com>
2013-05-17 15:43:14 +0200

    remove non-printable characters from checksum

--- bootstrap-core/src/test/java/de/agilecoders/wicket/core/request/resource/caching/version/Adler32ResourceVersionTest.java
+++ bootstrap-core/src/test/java/de/agilecoders/wicket/core/request/resource/caching/version/Adler32ResourceVersionTest.java
@@ -1,21 +1,43 @@
 package de.agilecoders.wicket.core.request.resource.caching.version;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * Tests the {@link Adler32ResourceVersion}
  *
  * @author miha
  */
+@RunWith(value = Parameterized.class)
 public class Adler32ResourceVersionTest extends ChecksumResourceVersionTest {
+
+    private final Map.Entry<String, String> content;
+
+    public Adler32ResourceVersionTest(final Map.Entry<String, String> content) {
+        this.content = content;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] {
+                { new AbstractMap.SimpleEntry<String, String>("a", "620062") },
+                { new AbstractMap.SimpleEntry<String, String>("a1", "f50093") },
+                { new AbstractMap.SimpleEntry<String, String>("a1b", "1ea00f5") },
+                { new AbstractMap.SimpleEntry<String, String>("a1bX", "337014d") },
+                { new AbstractMap.SimpleEntry<String, String>("a1bXl", "4f001b9") }
+        };
+        return Arrays.asList(data);
+    }
 
     @Test
     public void checkChecksum() {
-        check("a", "620062");
-        check("a1", "f50093");
-        check("a1b", "1ea00f5");
-        check("a1bX", "337014d");
-        check("a1bXl", "4f001b9");
+        check(content.getKey(), content.getValue());
     }
 
     @Override
--- bootstrap-core/src/test/java/de/agilecoders/wicket/core/request/resource/caching/version/CRC32ResourceVersionTest.java
+++ bootstrap-core/src/test/java/de/agilecoders/wicket/core/request/resource/caching/version/CRC32ResourceVersionTest.java
@@ -1,21 +1,43 @@
 package de.agilecoders.wicket.core.request.resource.caching.version;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
 
 /**
- * Tests the {@link de.agilecoders.wicket.core.request.resource.caching.version.Adler32ResourceVersion}
+ * Tests the {@link CRC32ResourceVersion}
  *
  * @author miha
  */
+@RunWith(value = Parameterized.class)
 public class CRC32ResourceVersionTest extends ChecksumResourceVersionTest {
+
+    private final Map.Entry<String, String> content;
+
+    public CRC32ResourceVersionTest(final Map.Entry<String, String> content) {
+        this.content = content;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] {
+                { new AbstractMap.SimpleEntry<String, String>("a", "620062") },
+                { new AbstractMap.SimpleEntry<String, String>("a1", "f50093") },
+                { new AbstractMap.SimpleEntry<String, String>("a1b", "1ea00f5") },
+                { new AbstractMap.SimpleEntry<String, String>("a1bX", "337014d") },
+                { new AbstractMap.SimpleEntry<String, String>("a1bXl", "4f001b9") }
+        };
+        return Arrays.asList(data);
+    }
 
     @Test
     public void checkChecksum() {
-        check("a", "e8b7be43");
-        check("a1", "6ce14823");
-        check("a1b", "d3b57fc3");
-        check("a1bX", "b50c103e");
-        check("a1bXl", "57d2d345");
+        check(content.getKey(), content.getValue());
     }
 
     @Override
