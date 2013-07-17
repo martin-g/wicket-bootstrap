0badfd6bb37ab92da064d39f71a5604526ee96ec
bhuism <meuk@odee.net>
2013-07-18 00:17:06 +0200

    Made the List items have the 'active' class when the page is selected in the PagingNavigator conform bootstrap example.

--- bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/navigation/BootstrapPagingNavigator.java
+++ bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/navigation/BootstrapPagingNavigator.java
@@ -1,14 +1,17 @@
 package de.agilecoders.wicket.core.markup.html.bootstrap.navigation;
 
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.list.LoopItem;
+import org.apache.wicket.markup.html.navigation.paging.IPageable;
+import org.apache.wicket.markup.html.navigation.paging.IPagingLabelProvider;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigation;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.model.Model;
+
 import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.BootstrapBaseBehavior;
 import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.ICssClassNameProvider;
 import de.agilecoders.wicket.core.util.Attributes;
-
-import org.apache.wicket.markup.ComponentTag;
-import org.apache.wicket.markup.html.navigation.paging.IPageable;
-import org.apache.wicket.markup.html.navigation.paging.IPagingLabelProvider;
-import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
-import org.apache.wicket.model.Model;
 
 /**
  * A Wicket panel component to draw and maintain a complete page navigator, meant to be easily added
@@ -61,7 +64,7 @@ public class BootstrapPagingNavigator extends PagingNavigator {
     }
 
     @Override
-    protected void onComponentTag(ComponentTag tag) {
+    protected void onComponentTag(final ComponentTag tag) {
         super.onComponentTag(tag);
 
         checkComponentTag(tag, "div");
@@ -69,12 +72,37 @@ public class BootstrapPagingNavigator extends PagingNavigator {
     }
 
     /**
+     * Create a new PagingNavigation. May be subclassed to make us of specialized PagingNavigation.
+     * 
+     * @param id
+     *            The id of the navigation component
+     * @param pageable
+     *            the pageable component
+     * @param labelProvider
+     *            The label provider for the link text.
+     * @return the navigation object
+     */
+    
+    @Override
+    protected PagingNavigation newNavigation(final String id, final IPageable pageable, final IPagingLabelProvider labelProvider) {
+	return new PagingNavigation(id, pageable, labelProvider) {
+	    @Override
+	    protected void populateItem(final LoopItem loopItem) {
+		super.populateItem(loopItem);
+		if ((getStartIndex() + loopItem.getIndex()) == pageable.getCurrentPage()) {
+		    loopItem.add(AttributeModifier.append("class", "active"));
+		}
+	    };
+	};
+    }
+    
+    /**
      * sets the position of the pagination component
      *
      * @param position The position
      * @return this instance for chaining
      */
-    public BootstrapPagingNavigator setPosition(Position position) {
+    public BootstrapPagingNavigator setPosition(final Position position) {
         positionModel.setObject(position.cssClassName());
         return this;
     }
