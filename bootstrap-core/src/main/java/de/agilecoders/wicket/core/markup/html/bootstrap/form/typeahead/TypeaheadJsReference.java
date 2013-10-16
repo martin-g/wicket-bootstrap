68346a56a81f4becc39b755c308a4ee22690dc06
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-10-16 11:02:37 +0300

    Issue #237 Use webjars-typeahead.js

--- bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/form/typeahead/TypeaheadJsReference.java
+++ bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/form/typeahead/TypeaheadJsReference.java
@@ -1,24 +1,22 @@
 package de.agilecoders.wicket.core.markup.html.bootstrap.form.typeahead;
 
 import de.agilecoders.wicket.core.markup.html.references.BootstrapJavaScriptReference;
+import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
 import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
-import org.apache.wicket.request.resource.JavaScriptResourceReference;
-import org.apache.wicket.request.resource.ResourceReference;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 /**
  *
  */
-public class TypeaheadJsReference extends JavaScriptResourceReference {
+public class TypeaheadJsReference extends WebjarsJavaScriptResourceReference {
 
     private static final TypeaheadJsReference INSTANCE = new TypeaheadJsReference();
 
     private TypeaheadJsReference() {
-        super(TypeaheadJsReference.class, "js/typeahead.js");
+        super("/typeaheadjs/current/typeahead.js");
     }
 
     @Override
