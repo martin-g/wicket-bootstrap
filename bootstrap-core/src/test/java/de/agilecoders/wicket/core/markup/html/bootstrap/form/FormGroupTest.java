b124ef9f48141d9feaadb6894caaaf4950628192
Thorsten Gilfert <thorsten.gilfert@googlemail.com>
2013-10-28 20:07:09 +0100

    Updated FormGroup to use the bootstrap3 CSS classes to show validation errors.

---
+++ bootstrap-core/src/test/java/de/agilecoders/wicket/core/markup/html/bootstrap/form/FormGroupTest.java
@@ -0,0 +1,128 @@
+package de.agilecoders.wicket.core.markup.html.bootstrap.form;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import org.apache.wicket.markup.Markup;
+import org.apache.wicket.markup.MarkupException;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.tester.FormTester;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import de.agilecoders.wicket.core.WicketApplicationTest;
+import de.agilecoders.wicket.core.test.IntegrationTest;
+
+@Category(IntegrationTest.class)
+public class FormGroupTest extends WicketApplicationTest {
+
+	@Test
+	public void isInstantiableWithoutError() {
+
+		FormGroup group = new FormGroup("id");
+
+		tester().startComponentInPage(group, Markup.of("<div wicket:id='id'>Group</div>"));
+		tester().assertNoErrorMessage();
+	}
+
+	@Test(expected = MarkupException.class)
+	public void tagNameIsAsserted() {
+
+		FormGroup group = new FormGroup("id");
+
+		tester().startComponentInPage(group, Markup.of("<span wicket:id='id'>Group</span>"));
+	}
+
+	@Test
+	public void formGroupWithInput() {
+
+		FormGroup group = new FormGroup("id");
+		TextField<String> input = new TextField<String>("value");
+		group.add(input);
+
+		tester().startComponentInPage(group,
+				Markup.of("<div wicket:id='id'><input type='text' wicket:id='value'/></div>"));
+		tester().assertNoErrorMessage();
+	}
+
+	@Test
+	public void formGroupSubmit() {
+
+		Model<FormData> model = Model.of(new FormData());
+		model.getObject();
+
+		Form<FormData> form = new Form<FormData>("form", new CompoundPropertyModel<FormData>(model));
+
+		FormGroup group = new FormGroup("id");
+		form.add(group);
+
+		TextField<String> input = new TextField<String>("value");
+		input.setRequired(true);
+		group.add(input);
+
+		tester().startComponentInPage(
+				form,
+				Markup.of("<form wicket:id='form'><div wicket:id='id'><input type='text' wicket:id='value'/></div></form>"));
+		FormTester formTester = tester().newFormTester("form", false);
+		formTester.setValue(input, "Hello World!");
+
+		formTester.submit();
+
+		tester().assertNoErrorMessage();
+		tester().assertNoInfoMessage();
+	}
+
+	@Test
+	public void formGroupSubmitValidation() {
+
+		tester().getSession().setLocale(Locale.ENGLISH); // for the validation message
+
+		Model<FormData> model = Model.of(new FormData());
+		model.getObject();
+
+		Form<FormData> form = new Form<FormData>("form", new CompoundPropertyModel<FormData>(model));
+
+		FormGroup group = new FormGroup("id");
+		form.add(group);
+
+		TextField<String> input = new TextField<String>("value");
+		input.setRequired(true);
+		group.add(input);
+
+		tester().startComponentInPage(
+				form,
+				Markup.of("<form wicket:id='form'><div wicket:id='id'><input type='text' wicket:id='value'/></div></form>"));
+		FormTester formTester = tester().newFormTester("form", false);
+
+		formTester.submit();
+
+		tester().assertLabel("form:id:error", "&#039;value&#039; is required.");
+		tester().assertContains("class=\".*has-error.*\""); //assert error CSS class is present
+	}
+
+	private class FormData implements Serializable {
+
+		private static final long	serialVersionUID	= 1L;
+
+		private String				value;
+
+		/**
+		 * @return Returns the value.
+		 */
+		public String getValue() {
+
+			return value;
+		}
+
+		/**
+		 * @param value The value to set.
+		 */
+		public void setValue(String value) {
+
+			this.value = value;
+		}
+	}
+}
