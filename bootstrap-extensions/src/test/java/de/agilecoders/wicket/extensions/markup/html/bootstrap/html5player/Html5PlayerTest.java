518a16d2a656581d3df8fa1c454d04d32cf32b3f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-12-17 10:16:33 +0200

    Replace occurrences of \n in the Html5PlayerTest with the current OS line separator.
    A user reported at users@wicket.apache.org that this test fails on Windows.

--- bootstrap-extensions/src/test/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/html5player/Html5PlayerTest.java
+++ bootstrap-extensions/src/test/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/html5player/Html5PlayerTest.java
@@ -3,6 +3,7 @@ package de.agilecoders.wicket.extensions.markup.html.bootstrap.html5player;
 import com.google.common.collect.Lists;
 import de.agilecoders.wicket.extensions.markup.html.bootstrap.WicketApplicationTest;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.util.string.Strings;
 import org.junit.Test;
 
 import java.util.List;
@@ -26,12 +27,14 @@ public class Html5PlayerTest extends WicketApplicationTest {
 
         tester().startComponentInPage(player);
 
-        tester().assertContains("<wicket:panel>\n"
+        String newLine = System.lineSeparator();
+
+        tester().assertContains(Strings.replaceAll("<wicket:panel>\n"
                                 + "            <video wicket:id=\"video\" width=\"370\" height=\"215\" id=\"video1\">\n"
                                 + "                <wicket:fragment wicket:id=\"videos\"><source wicket:id=\"element\" src=\"url\" type=\"type\"/></wicket:fragment>\n"
                                 + "                Your browser does not support the video tag.\n"
                                 + "            </video>\n"
-                                + "        </wicket:panel>");
+                                + "        </wicket:panel>", "\n", newLine).toString());
     }
 
 }
