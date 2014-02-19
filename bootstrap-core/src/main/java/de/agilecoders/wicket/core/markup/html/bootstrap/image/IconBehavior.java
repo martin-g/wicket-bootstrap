77de29fc6ada3edb5df0c6b578a10a96bf684315
Ernesto Reinaldo Barreiro <reiern70@gmail.com>
2014-02-19 11:43:54 +0100

    get rid of unused import

--- bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/image/IconBehavior.java
+++ bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/image/IconBehavior.java
@@ -1,6 +1,5 @@
 package de.agilecoders.wicket.core.markup.html.bootstrap.image;
 
-import de.agilecoders.wicket.core.markup.html.bootstrap.common.Invertible;
 import de.agilecoders.wicket.core.util.Attributes;
 import de.agilecoders.wicket.core.util.Components;
 
