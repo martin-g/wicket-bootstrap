b9fe8f7ecf8976909d0daf790ee9f4e7dd45a18d
Ernesto Reinaldo Barreiro <reiern70@gmail.com>
2014-03-07 13:42:04 +0100

    Allows to quickly build radio options as in: http://getbootstrap.com/javascript/#buttons

---
+++ bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/form/BootstrapRadioGroup.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package de.agilecoders.wicket.core.markup.html.bootstrap.form;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Radio;
+import org.apache.wicket.markup.html.form.RadioGroup;
+import org.apache.wicket.markup.html.panel.GenericPanel;
+import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.lang.Args;
+
+/**
+ * Allows to quickly build radio options as in
+ * 
+ * http://getbootstrap.com/javascript/#buttons
+ * 
+ * 
+ * @author reiern70
+ *
+ */
+public class BootstrapRadioGroup<T> extends GenericPanel<T> {
+
+	private  List<IModel<T>> options;
+	
+	private IRadioChoiceRenderer<T> choiceRenderer;
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param id
+	 */
+	public BootstrapRadioGroup(String id,  List<T> options, IRadioChoiceRenderer<T> choiceRenderer) {
+		this(id, null, options, choiceRenderer);
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param id
+	 * @param model
+	 */
+	public BootstrapRadioGroup(String id, IModel<T> model, List<T> options, IRadioChoiceRenderer<T> choiceRenderer) {
+		super(id, model);		
+		Args.notNull(choiceRenderer, "choiceRenderer");
+		this.choiceRenderer = choiceRenderer;
+		Args.notEmpty(options, "options");
+		this.options = new ArrayList<IModel<T>>();
+		for(T t: options) {
+			this.options.add(choiceRenderer.modelOf(t));
+		}
+	}
+	
+	@Override
+	protected void detachModel() {
+		super.detachModel();
+		for(IModel<T> model: options) {
+			model.detach();
+		}
+		choiceRenderer.detach();
+	}
+	
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();		
+		RadioGroup<T> radioGroup = new RadioGroup<T>("radioGroup", getModel());
+		radioGroup.setRenderBodyOnly(false);
+		add(radioGroup);
+		RepeatingView radios = new RepeatingView("radios");
+		radioGroup.add(radios);
+		for(final IModel<T> model: options) {
+			WebMarkupContainer wm = new WebMarkupContainer(radios.newChildId()) {
+				@Override
+				protected void onComponentTag(ComponentTag tag) {
+					super.onComponentTag(tag);
+					tag.put("class", "btn "+ choiceRenderer.getButtonClass(model.getObject()));
+				}
+			};
+			radios.add(wm);
+			wm.add(new Radio<T>("radio", model, radioGroup));
+			wm.add(new Label("label", choiceRenderer.lableOf(model.getObject())).setRenderBodyOnly(true));
+		}
+	}
+}
