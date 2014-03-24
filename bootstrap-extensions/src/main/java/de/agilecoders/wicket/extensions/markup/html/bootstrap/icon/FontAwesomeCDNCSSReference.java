0024d78bd0c0e567719b44b269ffe58bcd3b388e
David Beer <david.beer@copper-arrow.com>
2014-03-24 12:52:32 +0000

    Changed the Class to extend UrlResourceReference and pass the CDN url in the constructor

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/icon/FontAwesomeCDNCSSReference.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/icon/FontAwesomeCDNCSSReference.java
@@ -1,23 +1,19 @@
 package de.agilecoders.wicket.extensions.markup.html.bootstrap.icon;
 
 import org.apache.wicket.request.Url;
-import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.request.resource.UrlResourceReference;
 
 /**
  * reference for font awesome css via CDN
  */
-public class FontAwesomeCDNCSSReference {
+public class FontAwesomeCDNCSSReference extends UrlResourceReference{
     private static final long serialVersionUID = 1L;
-    private static String CDNURL = "//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css";
+    private static final String CDNURL = "//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css";
 
     /**
      * Singleton instance of this reference
      */
     private static final FontAwesomeCDNCSSReference INSTANCE = new FontAwesomeCDNCSSReference();
-
-    public FontAwesomeCDNCSSReference() {
-    }
 
     /**
      * @return the single instance of the resource reference
@@ -26,8 +22,8 @@ public class FontAwesomeCDNCSSReference {
         return INSTANCE;
     }
 
-    public ResourceReference getCDNURL() {
-        return new UrlResourceReference(Url.parse(CDNURL));
+    private FontAwesomeCDNCSSReference() {
+        super(Url.parse(CDNURL));
     }
-    
+
 }
