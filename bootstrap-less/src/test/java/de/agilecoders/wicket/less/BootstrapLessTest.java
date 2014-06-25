bb173843e310d2ca6c99e7cd059019c7b8c3f3b3
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-06-25 15:19:16 +0300

    Fixes #390 - Add support for dependencies in the classpath

--- bootstrap-less/src/test/java/de/agilecoders/wicket/less/BootstrapLessTest.java
+++ bootstrap-less/src/test/java/de/agilecoders/wicket/less/BootstrapLessTest.java
@@ -1,12 +1,17 @@
 package de.agilecoders.wicket.less;
 
+import com.github.sommeri.less4j.LessSource;
 import de.agilecoders.wicket.webjars.WicketWebjars;
 import org.apache.wicket.util.tester.WicketTester;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.net.URL;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -31,14 +36,30 @@ public class BootstrapLessTest {
         });
     }
 
+    @After
+    public void after() {
+        tester.destroy();
+    }
+
     @Test
-    public void testName() throws Exception {
+    public void importWebJars() throws Exception {
         LessCacheManager less = LessCacheManager.get();
         URL res = Thread.currentThread().getContextClassLoader().getResource("import.less");
 
-        assertTrue((".rule {\n"
-                   + "  background: #428bca;\n"
-                   + "}\n").equals(less.getCss(less.getLessSource(res))));
+        LessSource.URLSource lessSource = less.getLessSource(res);
+        String css = less.getCss(lessSource);
+        assertThat(css, is(equalTo(".rule {\n  background: #428bca;\n}\n")));
+
+    }
+
+    @Test
+    public void importClasspath() throws Exception {
+        LessCacheManager less = LessCacheManager.get();
+        URL res = Thread.currentThread().getContextClassLoader().getResource("importClasspath.less");
+
+        LessSource.URLSource lessSource = less.getLessSource(res);
+        String css = less.getCss(lessSource);
+        assertThat(css, is(equalTo(".classPathImported {\n  color: #333;\n}\n")));
 
     }
 }
