7073c18d5c26d94e64f12706fad9e7015f46361a
Michael Haitz <michael.haitz@agilecoders.de>
2014-07-16 16:46:37 +0200

    proposed solution for issue #414

--- bootstrap-core/src/test/java/de/agilecoders/wicket/core/util/DatesTest.java
+++ bootstrap-core/src/test/java/de/agilecoders/wicket/core/util/DatesTest.java
@@ -1,7 +1,6 @@
 package de.agilecoders.wicket.core.util;
 
 import de.agilecoders.wicket.core.test.TestCategory;
-import de.agilecoders.wicket.core.util.Dates;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -19,16 +18,16 @@ public class DatesTest {
 
     @Test
     public void toJavaScriptWorks() {
-        assertThat(Dates.toJavaScriptDateFormat("dd.MM.yyyy"), is(equalTo("dd.mm.yyyy")));
-        assertThat(Dates.toJavaScriptDateFormat("dd-MMMM-yyyy"), is(equalTo("dd-mmmm-yyyy")));
-        assertThat(Dates.toJavaScriptDateFormat("dd-MMM-yyyy"), is(equalTo("dd-mmM-yyyy")));
+        assertThat(Dates.toJavaScriptDateFormat("dd.MM.yyyy"), is(equalTo("d.m.yyyy")));
+        assertThat(Dates.toJavaScriptDateFormat("dd-MMMM-yyyy"), is(equalTo("d-M-yyyy")));
+        assertThat(Dates.toJavaScriptDateFormat("dd-MMM-yyyy"), is(equalTo("d-mm-yyyy")));
     }
 
     @Test
     public void toJavaWorks() {
-        assertThat(Dates.toJavaDateFormat("dd.mm.yyyy"), is(equalTo("dd.MM.yyyy")));
-        assertThat(Dates.toJavaDateFormat("dd-mmmm-yyyy"), is(equalTo("dd-MMMM-yyyy")));
-        assertThat(Dates.toJavaDateFormat("dd-mmm-yyyy"), is(equalTo("dd-MMm-yyyy")));
+        assertThat(Dates.toJavaDateFormat("dd.mm.yyyy"), is(equalTo("dd.m.yyyy")));
+        assertThat(Dates.toJavaDateFormat("dd-mmmm-yyyy"), is(equalTo("dd-mm-yyyy")));
+        assertThat(Dates.toJavaDateFormat("dd-mmm-yyyy"), is(equalTo("dd-mm-yyyy")));
     }
 
 }
