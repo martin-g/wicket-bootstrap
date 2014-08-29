8fb611dc696d06ca5465cdbe01fb61adc6c89c7f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-08-29 20:30:30 +0300

    [ladda] Add javadoc and clear the data in complete()

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/ladda/LaddaAjaxCallListener.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/ladda/LaddaAjaxCallListener.java
@@ -3,12 +3,16 @@ package de.agilecoders.wicket.extensions.markup.html.bootstrap.ladda;
 import org.apache.wicket.ajax.attributes.AjaxCallListener;
 
 /**
- *
+ * An {@link org.apache.wicket.ajax.attributes.IAjaxCallListener} that sets up Ladda for the
+ * duration of the Ajax call
  */
 public class LaddaAjaxCallListener extends AjaxCallListener {
 
+    /**
+     * Constructor
+     */
     public LaddaAjaxCallListener() {
         onBeforeSend("var $this = jQuery('#'+attrs.c); var l = Ladda.create($this[0]); l.start(); $this.data('ladda', l)");
-        onComplete("var $this = jQuery('#'+attrs.c); var l = $this.data('ladda'); if (l) {l.stop();}");
+        onComplete("var $this = jQuery('#'+attrs.c); var l = $this.data('ladda'); if (l) {l.stop(); $this.removeData('ladda');}");
     }
 }
