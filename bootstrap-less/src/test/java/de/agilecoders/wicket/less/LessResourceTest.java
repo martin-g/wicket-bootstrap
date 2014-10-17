9f722d44aed001b5828eb1d1082ff0feb07d6359
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-10-17 09:22:56 +0300

    [less] Make the test Windows line break resilient

--- bootstrap-less/src/test/java/de/agilecoders/wicket/less/LessResourceTest.java
+++ bootstrap-less/src/test/java/de/agilecoders/wicket/less/LessResourceTest.java
@@ -1,6 +1,7 @@
 package de.agilecoders.wicket.less;
 
 import org.apache.wicket.util.io.IOUtils;
+import org.apache.wicket.util.string.Strings;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,10 +22,12 @@ public class LessResourceTest extends Assert {
 
         WicketTester tester = new WicketTester(new TestApplication());
         tester.startResourceReference(new LessResourceReference(HomePage.class, "resources/root.less"));
-        String cssContent = tester.getLastResponseAsString();
+        CharSequence cssContent = tester.getLastResponseAsString();
+        cssContent = Strings.replaceAll(cssContent, "\r", "");
 
         InputStream expectedInputStream = LessResourceTest.class.getResourceAsStream("resources/expected.css");
-        String expected = IOUtils.toString(expectedInputStream);
+        CharSequence expected = IOUtils.toString(expectedInputStream);
+        expected = Strings.replaceAll(expected, "\r", "");
         assertEquals(expected, cssContent);
     }
 }
