7d073d155427e5bfe98b8056940aa9e764e6a7cc
lehvolk <lehvolk@gmail.com>
2014-11-15 22:41:31 +0300

    review fix

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/validation/TooltipValidation.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/validation/TooltipValidation.java
@@ -9,7 +9,7 @@ import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 /**
  * <h1>Validation behavior.</h1>
  * <p>Display errors in tooltips.</p>
- * This behavior may not properly with widgets like bootstrap-select or select2.
+ * This behavior may not work properly with widgets like bootstrap-select or select2.
  * In this case try to wrap all inputs in div (that's done by default in bootstrap form layout)
  * and set appendToParent of config to true.
  * <br/>
