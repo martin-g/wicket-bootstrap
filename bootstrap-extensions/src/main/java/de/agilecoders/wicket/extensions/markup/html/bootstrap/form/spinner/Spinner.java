0e109f8c89342dc4e0c98ac1624159f0db066caf
danjee <jipa.daniel@gmail.com>
2014-11-23 21:10:03 +0200

    added events listeners

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/spinner/Spinner.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/spinner/Spinner.java
@@ -2,6 +2,8 @@ package de.agilecoders.wicket.extensions.markup.html.bootstrap.form.spinner;
 
 import static de.agilecoders.wicket.jquery.JQuery.$;
 
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
@@ -11,6 +13,8 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.agilecoders.wicket.core.util.Attributes;
 
@@ -24,6 +28,8 @@ public class Spinner<T extends Number> extends TextField<T>{
 
 	private static final long serialVersionUID = -2660832209883037448L;
 
+	private static final Logger LOG = LoggerFactory.getLogger(Spinner.class);
+	
 	private static final CssResourceReference SPINNER_CSS = new CssResourceReference(Spinner.class,
 			"css/touchspin.min.css");
 	private static final JavaScriptResourceReference SPINNER_JS = new JavaScriptResourceReference(Spinner.class,
@@ -61,6 +67,135 @@ public class Spinner<T extends Number> extends TextField<T>{
 		response.render(CssHeaderItem.forReference(SPINNER_CSS));
 		response.render(JavaScriptHeaderItem.forReference(SPINNER_JS));
 		response.render(OnDomReadyHeaderItem.forScript(createScript(getConfig())));
+	}
+	
+	@Override
+	protected void onInitialize() {
+		addActionListeners();
+		super.onInitialize();
+	}
+	
+
+	private void addActionListeners() {
+		add(new AjaxEventBehavior("touchspin.on.startspin"){
+
+			private static final long serialVersionUID = 3911562409088754189L;
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				LOG.debug("Start spin");
+				onStartSpin();
+			}
+		});
+
+		add(new AjaxEventBehavior("touchspin.on.startupspin"){
+
+			private static final long serialVersionUID = 3911562409088754189L;
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				LOG.debug("Start up spin");
+				onStartUpSpin();
+			}
+		});
+		
+		add(new AjaxEventBehavior("touchspin.on.startdownspin"){
+
+			private static final long serialVersionUID = 3911562409088754189L;
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				LOG.debug("Start down spin");
+				onStartDownSpin();
+			}
+		});
+		
+		add(new AjaxEventBehavior("touchspin.on.stopspin"){
+
+			private static final long serialVersionUID = 3911562409088754189L;
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				LOG.debug("Stop spin");
+				onStopSpin();
+			}
+		});
+		
+		add(new AjaxEventBehavior("touchspin.on.stopupspin"){
+
+			private static final long serialVersionUID = 3911562409088754189L;
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				LOG.debug("Stop up spin");
+				onStopUpSpin();
+			}
+		});
+
+		add(new AjaxEventBehavior("touchspin.on.stopdownspin"){
+
+			private static final long serialVersionUID = 3911562409088754189L;
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				LOG.debug("Stop down spin");
+				onStopDownSpin();
+			}
+		});
+		
+		add(new AjaxEventBehavior("touchspin.on.min"){
+
+			private static final long serialVersionUID = 3911562409088754189L;
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				LOG.debug("Reached minimum value");
+				onMin();
+			}
+		});
+		
+		add(new AjaxEventBehavior("touchspin.on.max"){
+
+			private static final long serialVersionUID = 3911562409088754189L;
+
+			@Override
+			protected void onEvent(AjaxRequestTarget target) {
+				LOG.debug("Reached maximum value");
+				onMax();
+			}
+		});
+	}
+
+	protected void onMax() {
+
+	}
+
+	protected void onMin() {
+		
+	}
+
+	protected void onStopDownSpin() {
+		
+	}
+
+	protected void onStopUpSpin() {
+		
+	}
+
+	protected void onStopSpin() {
+		
+	}
+
+	protected void onStartDownSpin() {
+		
+	}
+
+	protected void onStartSpin() {
+		
+	}
+
+	protected void onStartUpSpin() {
+		
 	}
 
 	protected CharSequence createScript(final SpinnerConfig config) {
