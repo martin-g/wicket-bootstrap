ea7e635da7385ac6ed662a4ff2b6da03d354cbf7
danjee <jipa.daniel@gmail.com>
2014-11-23 18:49:37 +0200

    Added default values in configuration Also an ugly workaround in default keys max, min and step
    I think a modification has to be made in jquery-selector AbstractConfig
    so the use of 
    
    private static final IKey<? extends Number> max = newKey("max",100);
    
    will be valid

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/spinner/SpinnerConfig.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/spinner/SpinnerConfig.java
@@ -2,45 +2,58 @@ package de.agilecoders.wicket.extensions.markup.html.bootstrap.form.spinner;
 
 import de.agilecoders.wicket.jquery.AbstractConfig;
 import de.agilecoders.wicket.jquery.IKey;
-/**
- * @author daniel.jipa
- * @reference http://www.virtuosoft.eu/code/bootstrap-touchspin/
- */
+
+
 public class SpinnerConfig extends AbstractConfig {
 
 	private static final long serialVersionUID = 2239107002237150197L;
 
 	private static final IKey<Number> initval = newKey("initval", null);
 
-	private static final IKey<Number> max = newKey("max", null);
+	private static final IKey<Number> max = newKey("max", getDefaultValueAsNumber(100));
 
-	private static final IKey<Number> min = newKey("min", null);
+	private static final IKey<Number> min = newKey("min", getDefaultValueAsNumber(0));
 
-	private static final IKey<Number> step = newKey("step", null);
+	private static final IKey<Number> step = newKey("step", getDefaultValueAsNumber(1));
 
-	private static final IKey<Integer> boostat = newKey("boostat", null);
+	private static final IKey<String> forcestepdivisibility = newKey("forcestepdivisibility", "round");
 
-	private static final IKey<Integer> maxboostedstep = newKey("maxboostedstep", null);
+	private static final IKey<Integer> boostat = newKey("boostat", 10);
 
-	private static final IKey<Integer> decimals = newKey("decimals", null);
+	private static final IKey<Boolean> booster = newKey("booster", Boolean.TRUE);
+
+	private static final IKey<Integer> stepinterval = newKey("stepinterval", 100);
+
+	private static final IKey<Integer> stepintervaldelay = newKey("stepintervaldelay", 100);
+
+	private static final IKey<Boolean> maxboostedstep = newKey("maxboostedstep", Boolean.FALSE);
+
+	private static final IKey<Boolean> mousewheel = newKey("mousewheel", Boolean.TRUE);
+
+	private static final IKey<Integer> decimals = newKey("decimals", 0);
 
 	private static final IKey<String> buttondown_class = newKey("buttondown_class", null);
 
 	private static final IKey<String> buttonup_class = newKey("buttonup_class", null);
 
-	private static final IKey<Boolean> verticalbuttons = newKey("verticalbuttons", null);
+	private static final IKey<Boolean> verticalbuttons = newKey("verticalbuttons", Boolean.FALSE);
 
-	private static final IKey<String> verticalupclass = newKey("verticalupclass", null);
+	private static final IKey<String> verticalupclass = newKey("verticalupclass", "glyphicon glyphicon-chevron-up");
 
-	private static final IKey<String> verticaldownclass = newKey("verticaldownclass", null);
+	private static final IKey<String> verticaldownclass = newKey("verticaldownclass", "glyphicon glyphicon-chevron-down");
 
-	private static final IKey<String> prefix = newKey("prefix", null);
+	private static final IKey<String> prefix = newKey("prefix", "");
 
 	private static final IKey<String> prefix_extraclass = newKey("prefix_extraclass", null);
 
 	private static final IKey<String> postfix = newKey("postfix", null);
 
 	private static final IKey<String> postfix_extraclass = newKey("postfix_extraclass", null);
+
+
+	private static Number getDefaultValueAsNumber(Number value){
+		return value;
+	}
 
 	public SpinnerConfig withInitVal(Number value) {
 		put(initval, value);
@@ -62,13 +75,38 @@ public class SpinnerConfig extends AbstractConfig {
 		return this;
 	}
 
+	public SpinnerConfig withForcestepDivisibility(String value) {
+		put(forcestepdivisibility, value);
+		return this;
+	}
+
+	public SpinnerConfig withBootster(Boolean value) {
+		put(booster, value);
+		return this;
+	}
+
 	public SpinnerConfig withBootstap(Integer value) {
 		put(boostat, value);
 		return this;
 	}
 
-	public SpinnerConfig withMaxboostedstep(Integer value) {
+	public SpinnerConfig withStepInterval(Integer value) {
+		put(stepinterval, value);
+		return this;
+	}
+
+	public SpinnerConfig withStepIntervalDelay(Integer value) {
+		put(stepintervaldelay, value);
+		return this;
+	}
+
+	public SpinnerConfig withMaxboostedstep(Boolean value) {
 		put(maxboostedstep, value);
+		return this;
+	}
+
+	public SpinnerConfig withMouseWheel(Boolean value) {
+		put(mousewheel, value);
 		return this;
 	}
 
