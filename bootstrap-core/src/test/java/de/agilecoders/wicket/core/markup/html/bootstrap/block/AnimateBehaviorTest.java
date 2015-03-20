7672c85714edb7a60e0b882fa6b226253cfccdfc
danjee <jipa.daniel@gmail.com>
2015-03-20 15:54:26 +0200

    Added animated.css behavior Using CDN resources if bootstrap settings permits, otherwise a webjar resource

---
+++ bootstrap-core/src/test/java/de/agilecoders/wicket/core/markup/html/bootstrap/block/AnimateBehaviorTest.java
@@ -0,0 +1,20 @@
+package de.agilecoders.wicket.core.markup.html.bootstrap.block;
+
+import org.junit.Test;
+
+import de.agilecoders.wicket.core.WicketApplicationTest;
+import de.agilecoders.wicket.core.markup.html.bootstrap.block.AnimatedBehavior.Animation;
+
+/**
+ * @author daniel.jipa
+ *
+ */
+public class AnimateBehaviorTest extends WicketApplicationTest {
+
+    @Test
+    public void classNameWasAdded() {
+        assertCssClass(new AnimatedBehavior(Animation.bounce), "animate bounce");
+    }
+
+}
+
