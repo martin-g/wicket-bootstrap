9829164be248443397682a01788a1eddbaf48736
reiern70 <reiern70@gmail.com>
2015-12-31 16:50:11 +0100

    range slider example

--- bootstrap-samples/src/main/java/de/agilecoders/wicket/samples/pages/SliderPage.java
+++ bootstrap-samples/src/main/java/de/agilecoders/wicket/samples/pages/SliderPage.java
@@ -1,7 +1,9 @@
 package de.agilecoders.wicket.samples.pages;
 
 import de.agilecoders.wicket.core.markup.html.bootstrap.block.Code;
+import de.agilecoders.wicket.extensions.slider.BootstrapSlider;
 import de.agilecoders.wicket.extensions.slider.NumericBootstrapSlider;
+import de.agilecoders.wicket.extensions.slider.util.LongRangeValue;
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.Model;
@@ -18,6 +20,9 @@ import org.wicketstuff.annotation.mount.MountPath;
 public class SliderPage extends BasePage {
 
     private Model<Long> longSliderModel  = Model.of(100L);
+    
+    private Model<LongRangeValue> longRangeModel = Model.of( new LongRangeValue(10, 20));
+    
     /**
      * Construct.
      *
@@ -30,13 +35,22 @@ public class SliderPage extends BasePage {
         add(sliderForm);
         sliderForm.add(newSlider("slider", longSliderModel, 0L, 100000L, 10L));
         add(new Code("default-html-code", Model.of("//HTML\n<form><input wicket:id=\"slider\"></form>")).setShowLineNumbers(true),
-            new Code("default-java-code", Model.of("//JAVA\nadd(new DateTextField(\"default\"));")).setShowLineNumbers(true));
+            new Code("default-java-code", Model.of("//JAVA\nadd(new NumericBootstrapSlider<Long>(\"slider\")).setMin(min).setMax(max).setStep(step);")).setShowLineNumbers(true));
+
+        Form<Void> rangeForm = new Form<Void>("rangeForm");
+        add(rangeForm);
+        rangeForm.add(newRangeSlider("rangeSlider", longRangeModel, 0L, 100L, 5L));
+        add(new Code("range-html-code", Model.of("//HTML\n<form><input wicket:id=\"rangeSlider\"></form>")).setShowLineNumbers(true),
+                new Code("range-java-code", Model.of("//JAVA\nadd(new BootstrapSlider<LongRangeValue, Long>(\"rangeSlider\")).setMin(min).setMax(max).setStep(step);")).setShowLineNumbers(true));
     }
 
     private Component newSlider(String markupId, Model<Long> longSliderModel, Long min, Long max, Long step) {
         return new NumericBootstrapSlider<Long>(markupId, longSliderModel).setMin(min).setMax(max).setStep(step);
     }
 
+    private Component newRangeSlider(String markupId, Model<LongRangeValue> longSliderModel, Long min, Long max, Long step) {
+        return new BootstrapSlider<LongRangeValue, Long>(markupId, longSliderModel, LongRangeValue.class).setMin(min).setMax(max).setStep(step);
+    }
 
     @Override
     protected boolean hasNavigation() {
