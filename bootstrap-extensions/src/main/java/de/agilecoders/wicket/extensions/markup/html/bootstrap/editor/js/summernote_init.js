b81e9ddf9c7ad3fa83e314aff5c2f2d50be04cf2
Tobias Soloschenko <tsoloschenko@apache.org>
2016-01-29 17:45:04 +0100

    [summernote] Fixed image upload / spinner hide onload of image

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/editor/js/summernote_init.js
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/editor/js/summernote_init.js
@@ -11,56 +11,59 @@
     });
 
     var summernoteConfigDefault = {
-        toolbar : toolbar,
-        onImageUpload : function(files) {
-            var files = $(files);
-            var filesSize = files.length;
-            var overlay;
+		callbacks : {
+			onImageUpload : function(files) {
+	            var files = $(files);
+	            var filesSize = files.length;
+	            var overlay;
+	
+	            // Show Overlay
+	            var overlayTimeout = setTimeout(function() {
+	                overlay = $("<div class='summernoteOverlay'></div>").appendTo("body");
+	                new Spinner({color:'#fff'}).spin(overlay[0]);
+	            }, summernoteConfig.overlayTimeout);
+	
+	            files.each(function() {
+	                var file = this;
+	                var data = new FormData();
+	                data.append("file", file);
+	                url = summernoteConfig.imageUploadUrl;
+	                $.ajax({
+	                    data : data,
+	                    headers : {
+	                        "Wicket-Ajax" : "true",
+	                        "Wicket-Ajax-BaseURL" : Wicket.Ajax.baseUrl
+	                    },
+	                    type : "POST",
+	                    url : url,
+	                    cache : false,
+	                    contentType : false,
+	                    processData : false,
+	                    success : function(res, status, xhr) {
+	                        // Insert image
+	                        var imageUrl = xhr.getResponseHeader("imageUrl");
+	                        var decodedImageUrl = window.atob(/(image=)(.*)[^&]*/.exec(imageUrl)[2]);
+	                        imageUrl = imageUrl.replace(/(image=)[^&]*/, '$1' + decodedImageUrl);
+	                        $('#'+summernoteConfig.summernoteEditorId).summernote('insertImage', imageUrl, function($image){
+	                        	// after image load but image is not inserted yet.
+	                        	// https://github.com/summernote/summernote/issues/1472
+    	                        // Hide Overlay
+    	                        filesSize -= 1;
+    	                        if (!filesSize) {
+    	                            clearTimeout(overlayTimeout);
+    	                            if(overlay) {
+    	                                overlay.remove();
+    	                            }
+    	                        }
+	                        });
 
-            // Show Overlay
-            var overlayTimeout = setTimeout(function() {
-                overlay = $("<div class='summernoteOverlay'></div>").appendTo("body");
-                new Spinner({color:'#fff'}).spin(overlay[0]);
-            }, summernoteConfig.overlayTimeout);
-
-            files.each(function() {
-                var file = this;
-                var data = new FormData();
-                data.append("file", file);
-                url = summernoteConfig.imageUploadUrl;
-                $.ajax({
-                    data : data,
-                    headers : {
-                        "Wicket-Ajax" : "true",
-                        "Wicket-Ajax-BaseURL" : Wicket.Ajax.baseUrl
-                    },
-                    type : "POST",
-                    url : url,
-                    cache : false,
-                    contentType : false,
-                    processData : false,
-                    success : function(res, status, xhr) {
-                        // Insert image
-                        var imageUrl = xhr.getResponseHeader("imageUrl");
-                        var decodedImageUrl = window.atob(/(image=)(.*)[^&]*/.exec(imageUrl)[2]);
-                        imageUrl = imageUrl.replace(/(image=)[^&]*/, '$1' + decodedImageUrl);
-                        $('#'+summernoteConfig.summernoteEditorId).summernote('insertImage', imageUrl);
-
-                        // Hide Overlay
-                        filesSize -= 1;
-                        if (!filesSize) {
-                            clearTimeout(overlayTimeout);
-                            if(overlay) {
-                                overlay.remove();
-                            }
-                        }
-                    }
-                });
-            });
-        }
+	                    }
+	                });
+	            });
+	        }
+        },
+        toolbar : toolbar
     };
-
     $.extend(summernoteConfigDefault, summernoteConfig);
-
     summernote.summernote(summernoteConfigDefault);
 })();
