b812dc909fe6257364dabe8301c45b6d2a7b8264
Daniel Jipa <daniel.jipa@fortsoft.ro>
2016-05-14 01:38:46 +0300

    Update RatingCssResourceReference.java

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/rating/RatingCssResourceReference.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/rating/RatingCssResourceReference.java
@@ -2,13 +2,12 @@ package de.agilecoders.wicket.extensions.markup.html.bootstrap.form.rating;
 
 import org.apache.wicket.request.resource.PackageResourceReference;
 
-import de.agilecoders.wicket.extensions.slider.res.BootstrapSliderCssResourceReference;
 
 public class RatingCssResourceReference extends PackageResourceReference {
 
 	private static final long serialVersionUID = 1L;
 
-	private static final BootstrapSliderCssResourceReference INSTANCE = new BootstrapSliderCssResourceReference();
+	private static final RatingCssResourceReference INSTANCE = new RatingCssResourceReference();
 
     /**
      * Instantiates a new rating css resource reference.
@@ -22,7 +21,7 @@ public class RatingCssResourceReference extends PackageResourceReference {
      *
      * @return single instance of RatingCssResourceReference
      */
-    public static BootstrapSliderCssResourceReference getInstance() {
+    public static RatingCssResourceReference getInstance() {
         return INSTANCE;
     }
 }
