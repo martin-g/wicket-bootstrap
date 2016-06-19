9ec3c0f747657cd741ca1004c12fd709456000ea
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2016-06-19 10:19:07 +0200

    [DataTable] Fix a compilation problem due to API break in Wicket 7.0

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/table/sort/BootstrapOrderByBorder.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/table/sort/BootstrapOrderByBorder.java
@@ -8,7 +8,6 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.border.Border;
 
 import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.CssClassNameModifier;
-import de.agilecoders.wicket.core.markup.html.bootstrap.image.GlyphIconType;
 import de.agilecoders.wicket.core.markup.html.bootstrap.image.IconBehavior;
 import de.agilecoders.wicket.core.markup.html.bootstrap.image.IconType;
 
@@ -16,67 +15,55 @@ import de.agilecoders.wicket.core.markup.html.bootstrap.image.IconType;
  * A Bootstrap stylized component that wraps markup with an OrderByLink. This has the advantage of being able to add
  * the attribute modifier to the wrapping element as opposed to the link, so that it can be attached
  * to &lt;th&gt; or any other element.
- * 
+ *
  * A set of {@link IconType} must be provided.
- * 
+ *
  * For example:
- * 
+ *
  * &lt;th wicket:id="order-by-border"&gt;Heading&lt;/th&gt;
  *
  * @param <S> the type of the sorting parameter
  * @author Eric Hamel <eric.hamel@me.com>
- * 
+ *
  */
 public abstract class BootstrapOrderByBorder<S> extends Border {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Construct.
-	 * 
+	 *
 	 * @param id the component id
 	 * @param property the property to be sorted on
 	 * @param stateLocator the state locator
 	 */
 	public BootstrapOrderByBorder(final String id, final S property, final ISortStateLocator<S> stateLocator) {
-		this(id, property, stateLocator, new OrderByLink.DefaultCssProvider<S>());
-	}
-	
-	/**
-	 * Construct. 	
-	 * 
-	 * @param id the component id
-	 * @param property the property to be sorted on
-	 * @param stateLocator the state locator
-	 * @param cssProvider a custom css provider
-	 */
-	public BootstrapOrderByBorder(final String id, final S property, final ISortStateLocator<S> stateLocator, final OrderByLink.ICssProvider<S> cssProvider) {
 		super(id);
 
 		OrderByLink<S> link = newOrderByLink("orderByLink", property, stateLocator);
 		link.add(CssClassNameModifier.append("class", "show"));
-				
+
 		addToBorder(link);
-		add(new OrderByLink.CssModifier<S>(link, cssProvider));
+
 		link.add(getBodyContainer());
-		
+
 		WebMarkupContainer iconSort = new WebMarkupContainer("iconSort");
 		iconSort.setVisible(false);
 		link.add(iconSort);
-		
+
 		SortOrder sortOrder = stateLocator.getSortState().getPropertySortOrder(property);
-		
+
 		if(SortOrder.ASCENDING == sortOrder && ascendingIconType() != null){
-			
+
 			iconSort.setVisible(true);
 			iconSort.add(new IconBehavior(ascendingIconType()));
-			
+
 		}else if(SortOrder.DESCENDING == sortOrder && descendingIconType() != null){
-			
+
 			iconSort.setVisible(true);
 			iconSort.add(new IconBehavior(descendingIconType()));
-			
+
 		}else if(unsortedIconType() != null){
-			
+
 			iconSort.setVisible(true);
 			iconSort.add(new IconBehavior(unsortedIconType()));
 		}
@@ -84,15 +71,14 @@ public abstract class BootstrapOrderByBorder<S> extends Border {
 
 	/**
 	 * create new sort order toggling link
-	 * 
+	 *
 	 * @param id component id
 	 * @param property sort property
 	 * @param stateLocator sort state locator
 	 * @return link
 	 */
 	protected OrderByLink<S> newOrderByLink(final String id, final S property, final ISortStateLocator<S> stateLocator) {
-		return new OrderByLink<S>(id, property, stateLocator,
-			new OrderByLink.VoidCssProvider<S>())
+		return new OrderByLink<S>(id, property, stateLocator)
 		{
 			private static final long serialVersionUID = 1L;
 
