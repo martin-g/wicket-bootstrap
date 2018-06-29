1c2b1e622371da35b99b27dd07bef7a207aac0aa
Jan Ferko <julyloov@gmail.com>
2018-06-29 16:08:45 +0200

    Upgrade boostrap checkbox picker to work with v4
    We need to manually set invisible class to hide original input
    and default class to btn-light, because default classes were
    dropped in v4.

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/checkbox/bootstrapcheckbox/BootstrapCheckBoxPickerConfig.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/checkbox/bootstrapcheckbox/BootstrapCheckBoxPickerConfig.java
@@ -14,7 +14,7 @@ public class BootstrapCheckBoxPickerConfig extends AbstractConfig {
     private static final IKey<String> Style = newKey("style", null);
     private static final IKey<String> DefaultClass = newKey("defaultClass", "btn-secondary");
     private static final IKey<String> DisabledCursor = newKey("disabledCursor", "not-allowed");
-    private static final IKey<String> OffClass = newKey("offClass", "btn-danger ");
+    private static final IKey<String> OffClass = newKey("offClass", "btn-danger");
     private static final IKey<String> OnClass = newKey("onClass", "btn-success");
     private static final IKey<IconType> OffIconCLass = newKey("offIconClass", null);
     private static final IKey<IconType> OnIconCLass = newKey("onIconClass", null);
@@ -25,6 +25,7 @@ public class BootstrapCheckBoxPickerConfig extends AbstractConfig {
      * Default constructor
      */
     public BootstrapCheckBoxPickerConfig() {
+        withDefaultClass("btn-light");
     }
 
     public BootstrapCheckBoxPickerConfig withDefaultClass(final String value) {
