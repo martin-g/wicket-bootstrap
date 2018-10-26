b1ff8d7d944a015cf067211781b64b77c1a9dc71
helmut8080 <31631364+helmut8080@users.noreply.github.com>
2018-10-26 20:33:41 +0200

    Create DropDownVariationBehavior.java
    Variant for DropdownButton according to bootstrap 4

---
+++ bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/button/dropdown/DropDownVariationBehavior.java
@@ -0,0 +1,82 @@
+package de.agilecoders.wicket.core.markup.html.bootstrap.button.dropdown;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.lang.Args;
+
+import at.lerchen.wicket.bootstrap4.DropDownAlignmentBehavior.Alignment;
+import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.BootstrapBaseBehavior;
+import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.ICssClassNameProvider;
+import de.agilecoders.wicket.core.util.Attributes;
+
+public class DropDownVariationBehavior extends BootstrapBaseBehavior {
+
+	/**
+	 * Variant for DropdownButton defines if "drop" is done up, right, left or
+	 * default down
+	 */
+	public enum Variation implements ICssClassNameProvider {
+		DROPRIGHT("dropright"), DROPLEFT("dropleft"), DROPUP("dropup"), DROPDOWN("");
+		private String className;
+
+		/**
+		 * Construct.
+		 *
+		 * @param className
+		 *            the css class name
+		 */
+		Variation(final String className) {
+			this.className = className;
+		}
+
+		@Override
+		public String cssClassName() {
+			return className;
+		}
+	}
+
+	private IModel<Variation> alignment;
+
+	/**
+	 * Construct.
+	 *
+	 * @param alignment
+	 *            the alignment to use
+	 */
+	public DropDownVariationBehavior(final IModel<Variation> alignment) {
+		this.alignment = Args.notNull(alignment, "alignment");
+	}
+
+	/**
+	 * Construct.
+	 *
+	 * @param alignment
+	 *            the alignment to use
+	 */
+	public DropDownVariationBehavior(final Variation alignment) {
+		this(Model.of(Args.notNull(alignment, "alignment")));
+	}
+
+	@Override
+	public void onComponentTag(final Component component, final ComponentTag tag) {
+		super.onComponentTag(component, tag);
+
+		// remove existing alignment class names to allow switching alignment during
+		// ajax updates
+		Attributes.removeClass(tag, Variation.DROPLEFT.cssClassName(), Variation.DROPRIGHT.cssClassName(),
+				Variation.DROPUP.cssClassName());
+
+		Variation value = alignment.getObject();
+		switch (value) {
+		case DROPLEFT:
+		case DROPRIGHT:
+		case DROPUP:
+			Attributes.addClass(tag, value.cssClassName());
+		default:
+			break;
+		}
+	}
+
+}
