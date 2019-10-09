3f0fe7324be9811dec008ea55b6f5bb3bb76f97f
Vit Rozkovec <vit.rozkovec@gmail.com>
2019-10-09 07:20:18 +0200

    Bump BootstrapCheckboxPicker to v2.0.0 (#819)
    * Bump BootstrapCheckboxPicker to v2.0.0
    
    * Use method withSize(Size) of BootstrapCheckBoxPickerConfig

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/checkbox/bootstrapcheckbox/BootstrapCheckBoxPickerConfig.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/checkbox/bootstrapcheckbox/BootstrapCheckBoxPickerConfig.java
@@ -1,5 +1,6 @@
 package de.agilecoders.wicket.extensions.markup.html.bootstrap.form.checkbox.bootstrapcheckbox;
 
+import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.ICssClassNameProvider;
 import de.agilecoders.wicket.core.markup.html.bootstrap.button.ButtonGroup;
 import de.agilecoders.wicket.core.markup.html.bootstrap.image.IconType;
 import de.agilecoders.wicket.jquery.AbstractConfig;
@@ -11,16 +12,46 @@ import de.agilecoders.wicket.jquery.IKey;
  */
 public class BootstrapCheckBoxPickerConfig extends AbstractConfig {
 
-    private static final IKey<String> Style = newKey("style", null);
-    private static final IKey<String> DefaultClass = newKey("defaultClass", "btn-default");
     private static final IKey<String> DisabledCursor = newKey("disabledCursor", "not-allowed");
-    private static final IKey<String> OffClass = newKey("offClass", "btn-danger ");
-    private static final IKey<String> OnClass = newKey("onClass", "btn-success");
-    private static final IKey<IconType> OffIconCLass = newKey("offIconClass", null);
-    private static final IKey<IconType> OnIconCLass = newKey("onIconClass", null);
+    private static final IKey<String> DefaultClass = newKey("cls", null);
+    private static final IKey<String> OffClass = newKey("offCls", "btn-danger ");
+    private static final IKey<String> OnClass = newKey("onCls", "btn-success");
+    private static final IKey<IconType> OffIconCLass = newKey("offIconCls", null);
+    private static final IKey<IconType> OnIconCLass = newKey("onIconCls", null);
     private static final IKey<Boolean> Reverse = newKey("reverse", false);
     private static final IKey<String[]> ToggleKeyCodes = newKey("toggleKeyCodes", null);
+    private static final IKey<String> GroupCls = newKey("groupCls", null);
 
+    /**
+     * Defines all possible button types.
+     * @see <a href="http://getbootstrap.com/css/#buttons">Buttons</a>
+     */
+    public enum Size implements ICssClassNameProvider {
+        Default(null),
+        Small("btn-group-sm"),
+        Large("btn-group-lg");
+
+        private final String cssClassName;
+
+        /**
+         * Construct.
+         *
+         * @param cssClassName the css class name of button type
+         */
+        Size(String cssClassName) {
+            this.cssClassName = cssClassName;
+        }
+
+        /**
+         * @return css class name of button type
+         */
+        @Override
+        public String cssClassName() {
+            return cssClassName;
+        }
+
+    }
+    
     /**
      * Default constructor
      */
@@ -57,11 +88,6 @@ public class BootstrapCheckBoxPickerConfig extends AbstractConfig {
         return this;
     }
 
-    public BootstrapCheckBoxPickerConfig withStyle(final ButtonGroup.Size value) {
-        put(Style, value.cssClassName());
-        return this;
-    }
-
     public BootstrapCheckBoxPickerConfig withReverse(final boolean value) {
         put(Reverse, value);
         return this;
@@ -70,5 +96,10 @@ public class BootstrapCheckBoxPickerConfig extends AbstractConfig {
     public BootstrapCheckBoxPickerConfig withToggleKeyCodes(final String[] value) {
         put(ToggleKeyCodes, value);
         return this;
+    }
+    
+    public BootstrapCheckBoxPickerConfig withSize(final Size size) {
+    	put(GroupCls, size.cssClassName());
+    	return this;
     }
 }
