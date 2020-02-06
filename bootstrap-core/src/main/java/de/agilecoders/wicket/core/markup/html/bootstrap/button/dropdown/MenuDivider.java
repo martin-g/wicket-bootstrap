0bfe6301151f142dbd6583b078015e302992c456
Maxim Solodovnik <solomax666@gmail.com>
2020-02-07 00:05:15 +0700

    Dropdowns work better in CSP, menu divider is fixed

--- bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/button/dropdown/MenuDivider.java
+++ bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/button/dropdown/MenuDivider.java
@@ -1,10 +1,9 @@
 package de.agilecoders.wicket.core.markup.html.bootstrap.button.dropdown;
 
-import de.agilecoders.wicket.core.markup.html.bootstrap.button.ButtonList;
-
-import de.agilecoders.wicket.core.util.ListItemCssClassHelper;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.link.AbstractLink;
-import org.apache.wicket.model.Model;
+
+import de.agilecoders.wicket.core.markup.html.bootstrap.button.ButtonList;
 
 /**
  * A simple divider for menu elements.
@@ -12,28 +11,19 @@ import org.apache.wicket.model.Model;
  * @author miha
  */
 public class MenuDivider extends AbstractLink {
+    private static final long serialVersionUID = 1L;
 
     /**
      * Construct.
      */
     public MenuDivider() {
         super(ButtonList.getButtonMarkupId());
-
-        setBody(Model.of("&nbsp;"));
-        setEscapeModelStrings(false);
     }
 
     @Override
-    protected void onInitialize() {
-        super.onInitialize();
-
-        ListItemCssClassHelper.onInitialize(this, "dropdown-divider");
-    }
-
-    @Override
-    protected void onConfigure() {
-        super.onConfigure();
-
-        ListItemCssClassHelper.onConfigure(this, "dropdown-divider");
+    protected void onComponentTag(ComponentTag tag) {
+        tag.setName("div");
+        tag.getAttributes().clear();
+        tag.append("class", "dropdown-divider", " ");
     }
 }
