798ea5d1ecfec23c02f63dbcb3ac08dfc7861b66
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-04-13 08:23:05 +0300

    Update dependencies
    Disable one test until Wicket 9.0.0-M5+ is released
    Fix SASS related test after updating Sass version

--- bootstrap-core/src/test/java/de/agilecoders/wicket/core/markup/html/bootstrap/navbar/NavbarTest.java
+++ bootstrap-core/src/test/java/de/agilecoders/wicket/core/markup/html/bootstrap/navbar/NavbarTest.java
@@ -8,6 +8,7 @@ import org.apache.wicket.Page;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.util.tester.TagTester;
 import org.apache.wicket.util.visit.IVisitor;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -90,6 +91,7 @@ class NavbarTest extends WicketApplicationTest {
         assertTrue(classes.contains("sticky-top"));
     }
 
+    @Disabled // until Wicket 9.0.0-M5+
     @Test
     void initialLeftNavigationIsEmpty() {
         Navbar navbar = new Navbar("id");
@@ -98,7 +100,7 @@ class NavbarTest extends WicketApplicationTest {
         System.err.println(tester().getLastResponseAsString());
 
         TagTester tagTester = tester().getTagByWicketId("collapse");
-        TagTester ulTag = tagTester.getChild("style", "display:none");
+        TagTester ulTag = tagTester.getChild("hidden", "");
 
         assertThat(ulTag.getValue(), is(equalTo("")));
         assertThat(ulTag.getName(), is(equalTo("ul")));
