c43918a7bd2673b6a194174111a398cca2374326
Igor B <igorky@gmail.com>
2020-04-24 15:56:01 -0500

    #858 BootstrapTabbedPanel has missing css active attribute

--- bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/tabs/BootstrapTabbedPanel.java
+++ bootstrap-core/src/main/java/de/agilecoders/wicket/core/markup/html/bootstrap/tabs/BootstrapTabbedPanel.java
@@ -2,9 +2,12 @@ package de.agilecoders.wicket.core.markup.html.bootstrap.tabs;
 
 import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.BootstrapBaseBehavior;
 
+import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.CssClassNameAppender;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel;
 import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.IModel;
 
 import java.util.List;
@@ -48,4 +51,29 @@ public class BootstrapTabbedPanel<T extends ITab> extends TabbedPanel<T> {
 	{
 		return "nav nav-tabs";
 	}
+
+    @Override
+    protected WebMarkupContainer newLink(final String linkId, final int index) {
+        return new Link<Void>(linkId) {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void onConfigure() {
+                super.onConfigure();
+
+                final int currentlyActiveTab = BootstrapTabbedPanel.this.getSelectedTab();
+
+                if (index == currentlyActiveTab) {
+                    // Add the .active class to the <a>
+                    add(new CssClassNameAppender("active"));
+                }
+            }
+
+            @Override
+            public void onClick() {
+                setSelectedTab(index);
+            }
+        };
+    }
 }
