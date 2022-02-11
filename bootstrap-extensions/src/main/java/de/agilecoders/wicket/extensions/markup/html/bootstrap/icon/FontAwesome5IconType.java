0dc770fb9fabef0bfb206ac8d7b57c29c581106d
Maxim Solodovnik <solomax666@gmail.com>
2022-02-11 14:14:18 +0700

    Dependencies are updated according to release

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/icon/FontAwesome5IconType.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/icon/FontAwesome5IconType.java
@@ -17,7 +17,7 @@ import de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome5I
  *  &lt;dependency&gt;<br/>
  *      &lt;groupId&gt;org.webjars&lt;/groupId&gt;<br/>
  *      &lt;artifactId&gt;font-awesome&lt;/artifactId&gt;<br/>
- *      &lt;version&gt;5.15.4&lt;/version&gt;<br/>
+ *      &lt;version&gt;6.0.0&lt;/version&gt;<br/>
  *  &lt;/dependency&gt;<br/>
  *
  * All the icon types.
