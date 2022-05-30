434a346d1c895ab2e9c1fa6b85c7781986cf6397
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2022-05-30 14:00:46 +0300

    Issue #947 - Calculate the path to the fonts on the fly
    Add `.min` for some resources because the Webjar does not contain the
    non-minified resources.
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- bootstrap-themes/src/main/java/de/agilecoders/wicket/themes/markup/html/italia/ItaliaTheme.java
+++ bootstrap-themes/src/main/java/de/agilecoders/wicket/themes/markup/html/italia/ItaliaTheme.java
@@ -12,11 +12,13 @@ import org.apache.wicket.markup.head.PriorityHeaderItem;
 import de.agilecoders.wicket.core.settings.Theme;
 import de.agilecoders.wicket.webjars.request.resource.WebjarsCssResourceReference;
 import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
+import org.apache.wicket.request.Url;
+import org.apache.wicket.request.cycle.RequestCycle;
 
 /**
  * A {@link de.agilecoders.wicket.core.settings.ITheme theme} for Bootstrap
  * provided by <a href="https://developers.italia.it/">developers.italia.it</a>.
- * 
+ *
  * <h2>Usage</h2>
  * <pre>settings.setThemeProvider(new SingleThemeProvider(new ItaliaTheme()));</pre>
  *
@@ -30,35 +32,40 @@ public class ItaliaTheme extends Theme {
     private ItaliaTheme(final String name) {
     	super(name);
     }
-    
+
     /**
      * Construct.
      */
     public ItaliaTheme() {
         this("italia");
     }
-    
+
     @Override
     public List<HeaderItem> getDependencies() {
-		List<HeaderItem> references = new ArrayList<HeaderItem>();
+		List<HeaderItem> references = new ArrayList<>();
 		references.add(
-				CssHeaderItem.forReference(new WebjarsCssResourceReference("splide/current/css/splide-core.css")));
+				CssHeaderItem.forReference(new WebjarsCssResourceReference("splide/current/css/splide-core.min.css")));
 		references.add(CssHeaderItem
 				.forReference(
-						new WebjarsCssResourceReference("bootstrap-italia/current/css/bootstrap-italia.css"))
+						new WebjarsCssResourceReference("bootstrap-italia/current/css/bootstrap-italia.min.css"))
 				.setId(BOOTSTRAP_THEME_MARKUP_ID));
 		references.add(new PriorityHeaderItem(JavaScriptHeaderItem
 				.forReference(Application.get().getJavaScriptLibrarySettings().getJQueryReference())));
 		references.add(JavaScriptHeaderItem.forReference(
 				new WebjarsJavaScriptResourceReference("popper.js/current/umd/popper.js")));
-		references.add(JavaScriptHeaderItem.forReference(
-				new WebjarsJavaScriptResourceReference("splide/current/js/splide.js")));
-		references.add(JavaScriptHeaderItem.forScript("window.__PUBLIC_PATH__ = '/wicket/resource/de.agilecoders.wicket.webjars.request.resource.IWebjarsResourceReference/webjars/bootstrap-italia/current/fonts'", "bootstrap-italia-fonts"));
-		references.add(JavaScriptHeaderItem
-				.forReference(
-						new WebjarsJavaScriptResourceReference("bootstrap-italia/current/js/bootstrap-italia.js"))
-				.setId("bootstrap-js"));
+		references.add(JavaScriptHeaderItem.forReference(new WebjarsJavaScriptResourceReference("splide/current/js/splide.js")));
+
+		WebjarsJavaScriptResourceReference italiaJsReference = new WebjarsJavaScriptResourceReference("bootstrap-italia/current/js/bootstrap-italia.min.js");
+
+		CharSequence urlForJs = RequestCycle.get().urlFor(italiaJsReference, null);
+
+		Url fontsUrl = Url.parse(urlForJs);
+		fontsUrl.resolveRelative(Url.parse("../fonts"));
+
+		references.add(JavaScriptHeaderItem.forScript("window.__PUBLIC_PATH__ = '"+fontsUrl+"'", "bootstrap-italia-fonts"));
+		references.add(JavaScriptHeaderItem.forReference(italiaJsReference).setId("bootstrap-js"));
+
 		return references;
-    }
+	}
 
 }
