d540781f06e9db885a61b8143bf6216a728ae3be
Urs Joss <urs.joss@gmx.ch>
2022-06-02 00:31:54 +0200

    Restore FontAwesome5 classes

--- bootstrap-extensions/src/test/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/icon/FontAwesome5IconTypeTest.java
+++ bootstrap-extensions/src/test/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/icon/FontAwesome5IconTypeTest.java
@@ -1,103 +1,103 @@
 package de.agilecoders.wicket.extensions.markup.html.bootstrap.icon;
 
-import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.on;
-import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.FontAwesome6Brand._500px;
-import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.FontAwesome6Regular.envelope_open;
-import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.FontAwesome6Solid.address_book;
-import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.FontAwesome6Solid.align_center;
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome5IconTypeBuilder.on;
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome5IconTypeBuilder.FontAwesome5Brand._500px;
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome5IconTypeBuilder.FontAwesome5Regular.envelope_open;
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome5IconTypeBuilder.FontAwesome5Solid.address_book;
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome5IconTypeBuilder.FontAwesome5Solid.align_center;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 /**
- * Tests for {@link FontAwesome6IconType}
+ * Tests for {@link FontAwesome5IconType}
  */
 class FontAwesome5IconTypeTest {
 
     @Test
     void cssClassName() {
-        assertEquals("fa-solid fa-circle-half-stroke", FontAwesome6IconType.circle_half_stroke_s.cssClassName(), "FontAwesome should use 'fa-solid fa-NAME");
+        assertEquals("fas fa-adjust", FontAwesome5IconType.adjust_s.cssClassName(), "FontAwesome should use 'fas fa-NAME");
     }
 
     @Test
     void cssClassNameFixedWidthAlias() {
-        FontAwesome6IconType fontAwesomeIcon = on(align_center)
+        FontAwesome5IconType fontAwesomeIcon = on(align_center)
             .fw()
             .build();
-        assertEquals("fa-solid fa-align-center fa-fw", fontAwesomeIcon.cssClassName(),
-            "FontAwesome should use 'fa-solid fa-NAME fa-fw");
+        assertEquals("fas fa-align-center fa-fw", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fas fa-NAME fa-fw");
     }
 
     @Test
     void cssClassNameFixedWidth() {
-        FontAwesome6IconType fontAwesomeIcon = on(align_center)
+        FontAwesome5IconType fontAwesomeIcon = on(align_center)
             .fixedWidth()
             .build();
-        assertEquals("fa-solid fa-align-center fa-fw", fontAwesomeIcon.cssClassName(),
-            "FontAwesome should use 'fa-solid fa-NAME fa-fw");
+        assertEquals("fas fa-align-center fa-fw", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fas fa-NAME fa-fw");
     }
 
     @Test
     void cssClassSpin() {
-        FontAwesome6IconType fontAwesomeIcon = on(align_center)
+        FontAwesome5IconType fontAwesomeIcon = on(align_center)
             .spin()
             .build();
-        assertEquals("fa-solid fa-align-center fa-spin", fontAwesomeIcon.cssClassName(),
-            "FontAwesome should use 'fa-solid fa-NAME fa-spin");
+        assertEquals("fas fa-align-center fa-spin", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fas fa-NAME fa-spin");
     }
 
     @Test
     void cssClassRotate() {
-        FontAwesome6IconType fontAwesomeIcon = on(align_center)
-            .rotate(FontAwesome6IconTypeBuilder.Rotation.rotate_90)
+        FontAwesome5IconType fontAwesomeIcon = on(align_center)
+            .rotate(FontAwesome5IconTypeBuilder.Rotation.rotate_90)
             .build();
-        assertEquals("fa-solid fa-align-center fa-rotate-90", fontAwesomeIcon.cssClassName(),
-            "FontAwesome should use 'fa-solid fa-NAME fa-ROTATE");
+        assertEquals("fas fa-align-center fa-rotate-90", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fas fa-NAME fa-ROTATE");
     }
 
     @Test
     void cssClassSize() {
-        FontAwesome6IconType fontAwesomeIcon = on(align_center)
-            .size(FontAwesome6IconTypeBuilder.Size.two)
+        FontAwesome5IconType fontAwesomeIcon = on(align_center)
+            .size(FontAwesome5IconTypeBuilder.Size.two)
             .build();
-        assertEquals("fa-solid fa-align-center fa-2x", fontAwesomeIcon.cssClassName(),
-            "FontAwesome should use 'fa-solid fa-NAME fa-SIZE");
+        assertEquals("fas fa-align-center fa-2x", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fas fa-NAME fa-SIZE");
     }
 
     @Test
     void allAttributes() {
-        FontAwesome6IconType fontAwesomeIcon = on(address_book)
+        FontAwesome5IconType fontAwesomeIcon = on(address_book)
             .spin()
             .fw()
-            .rotate(FontAwesome6IconTypeBuilder.Rotation.flip_horizontal)
-            .size(FontAwesome6IconTypeBuilder.Size.large)
+            .rotate(FontAwesome5IconTypeBuilder.Rotation.flip_horizontal)
+            .size(FontAwesome5IconTypeBuilder.Size.large)
             .build();
-        assertEquals("fa-solid fa-address-book fa-spin fa-fw fa-flip-horizontal fa-lg", fontAwesomeIcon.cssClassName(),
-            "FontAwesome should use 'fa-solid fa-NAME fa-spin fa-fw fa-ROTATE fa-SIZE");
+        assertEquals("fas fa-address-book fa-spin fa-fw fa-flip-horizontal fa-lg", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fas fa-NAME fa-spin fa-fw fa-ROTATE fa-SIZE");
     }
 
 
     @Test
     void withRegularStyle_allAttributes() {
-        FontAwesome6IconType fontAwesomeIcon = on(envelope_open)
+        FontAwesome5IconType fontAwesomeIcon = on(envelope_open)
             .spin()
             .fw()
-            .rotate(FontAwesome6IconTypeBuilder.Rotation.flip_horizontal)
-            .size(FontAwesome6IconTypeBuilder.Size.large)
+            .rotate(FontAwesome5IconTypeBuilder.Rotation.flip_horizontal)
+            .size(FontAwesome5IconTypeBuilder.Size.large)
             .build();
-        assertEquals("fa-regular fa-envelope-open fa-spin fa-fw fa-flip-horizontal fa-lg", fontAwesomeIcon.cssClassName(),
-            "FontAwesome should use 'fa-regular fa-NAME fa-spin fa-fw fa-ROTATE fa-SIZE");
+        assertEquals("far fa-envelope-open fa-spin fa-fw fa-flip-horizontal fa-lg", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'far fa-NAME fa-spin fa-fw fa-ROTATE fa-SIZE");
     }
 
     @Test
     void withBrandIcon_allAttributes() {
-        FontAwesome6IconType fontAwesomeIcon = on(_500px)
+        FontAwesome5IconType fontAwesomeIcon = on(_500px)
             .spin()
             .fw()
-            .rotate(FontAwesome6IconTypeBuilder.Rotation.flip_horizontal)
-            .size(FontAwesome6IconTypeBuilder.Size.large)
+            .rotate(FontAwesome5IconTypeBuilder.Rotation.flip_horizontal)
+            .size(FontAwesome5IconTypeBuilder.Size.large)
             .build();
-        assertEquals("fa-brands fa-500px fa-spin fa-fw fa-flip-horizontal fa-lg", fontAwesomeIcon.cssClassName(),
-            "FontAwesome should use 'fa-brands fab-NAME fa-spin fa-fw fa-ROTATE fa-SIZE");
+        assertEquals("fab fa-500px fa-spin fa-fw fa-flip-horizontal fa-lg", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fab fab-NAME fa-spin fa-fw fa-ROTATE fa-SIZE");
     }
 }
---
+++ bootstrap-extensions/src/test/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/icon/FontAwesome6IconTypeTest.java
@@ -0,0 +1,103 @@
+package de.agilecoders.wicket.extensions.markup.html.bootstrap.icon;
+
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.on;
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.FontAwesome6Brand._500px;
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.FontAwesome6Regular.envelope_open;
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.FontAwesome6Solid.address_book;
+import static de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6IconTypeBuilder.FontAwesome6Solid.align_center;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link FontAwesome6IconType}
+ */
+class FontAwesome6IconTypeTest {
+
+    @Test
+    void cssClassName() {
+        assertEquals("fa-solid fa-circle-half-stroke", FontAwesome6IconType.circle_half_stroke_s.cssClassName(), "FontAwesome should use 'fa-solid fa-NAME");
+    }
+
+    @Test
+    void cssClassNameFixedWidthAlias() {
+        FontAwesome6IconType fontAwesomeIcon = on(align_center)
+            .fw()
+            .build();
+        assertEquals("fa-solid fa-align-center fa-fw", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fa-solid fa-NAME fa-fw");
+    }
+
+    @Test
+    void cssClassNameFixedWidth() {
+        FontAwesome6IconType fontAwesomeIcon = on(align_center)
+            .fixedWidth()
+            .build();
+        assertEquals("fa-solid fa-align-center fa-fw", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fa-solid fa-NAME fa-fw");
+    }
+
+    @Test
+    void cssClassSpin() {
+        FontAwesome6IconType fontAwesomeIcon = on(align_center)
+            .spin()
+            .build();
+        assertEquals("fa-solid fa-align-center fa-spin", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fa-solid fa-NAME fa-spin");
+    }
+
+    @Test
+    void cssClassRotate() {
+        FontAwesome6IconType fontAwesomeIcon = on(align_center)
+            .rotate(FontAwesome6IconTypeBuilder.Rotation.rotate_90)
+            .build();
+        assertEquals("fa-solid fa-align-center fa-rotate-90", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fa-solid fa-NAME fa-ROTATE");
+    }
+
+    @Test
+    void cssClassSize() {
+        FontAwesome6IconType fontAwesomeIcon = on(align_center)
+            .size(FontAwesome6IconTypeBuilder.Size.two)
+            .build();
+        assertEquals("fa-solid fa-align-center fa-2x", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fa-solid fa-NAME fa-SIZE");
+    }
+
+    @Test
+    void allAttributes() {
+        FontAwesome6IconType fontAwesomeIcon = on(address_book)
+            .spin()
+            .fw()
+            .rotate(FontAwesome6IconTypeBuilder.Rotation.flip_horizontal)
+            .size(FontAwesome6IconTypeBuilder.Size.large)
+            .build();
+        assertEquals("fa-solid fa-address-book fa-spin fa-fw fa-flip-horizontal fa-lg", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fa-solid fa-NAME fa-spin fa-fw fa-ROTATE fa-SIZE");
+    }
+
+
+    @Test
+    void withRegularStyle_allAttributes() {
+        FontAwesome6IconType fontAwesomeIcon = on(envelope_open)
+            .spin()
+            .fw()
+            .rotate(FontAwesome6IconTypeBuilder.Rotation.flip_horizontal)
+            .size(FontAwesome6IconTypeBuilder.Size.large)
+            .build();
+        assertEquals("fa-regular fa-envelope-open fa-spin fa-fw fa-flip-horizontal fa-lg", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fa-regular fa-NAME fa-spin fa-fw fa-ROTATE fa-SIZE");
+    }
+
+    @Test
+    void withBrandIcon_allAttributes() {
+        FontAwesome6IconType fontAwesomeIcon = on(_500px)
+            .spin()
+            .fw()
+            .rotate(FontAwesome6IconTypeBuilder.Rotation.flip_horizontal)
+            .size(FontAwesome6IconTypeBuilder.Size.large)
+            .build();
+        assertEquals("fa-brands fa-500px fa-spin fa-fw fa-flip-horizontal fa-lg", fontAwesomeIcon.cssClassName(),
+            "FontAwesome should use 'fa-brands fab-NAME fa-spin fa-fw fa-ROTATE fa-SIZE");
+    }
+}
