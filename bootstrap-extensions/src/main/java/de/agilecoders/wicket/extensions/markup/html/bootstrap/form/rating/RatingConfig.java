02cda1a39090e34ebd54ebcd168829694782a1c1
Urs Joss <urs.joss@gmx.ch>
2022-06-06 17:40:17 +0200

    revert some accidential white-space changes

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/rating/RatingConfig.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/rating/RatingConfig.java
@@ -10,67 +10,67 @@ import org.apache.wicket.Application;
  */
 public class RatingConfig extends AbstractConfig {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private static final IKey<Integer> start = newKey("start", 0);
+	private static final IKey<Integer> start = newKey("start", 0);
 
-    private static final IKey<Integer> stop = newKey("stop", 5);
+	private static final IKey<Integer> stop = newKey("stop", 5);
 
-    private static final IKey<Integer> step = newKey("step", 1);
+	private static final IKey<Integer> step = newKey("step", 1);
 
-    private static final IKey<Integer> fractions = newKey("fractions", 1);
+	private static final IKey<Integer> fractions = newKey("fractions", 1);
 
-    private static final IKey<String> filled = newKey("filled", null);
+	private static final IKey<String> filled = newKey("filled", null);
 
-    private static final IKey<String> empty = newKey("empty", null);
+	private static final IKey<String> empty = newKey("empty", null);
 
-    private static final IKey<String> filledSelected = newKey("filledSelected", null);
+	private static final IKey<String> filledSelected = newKey("filledSelected", null);
 
-    public RatingConfig() {
-        put(filled, getCssClassNameFor(FontAwesomeSettings.IconKey.FILLED));
-        put(empty, getCssClassNameFor(FontAwesomeSettings.IconKey.EMPTY));
-    }
+	public RatingConfig() {
+		put(filled, getCssClassNameFor(FontAwesomeSettings.IconKey.FILLED));
+		put(empty, getCssClassNameFor(FontAwesomeSettings.IconKey.EMPTY));
+	}
 
-    private String getCssClassNameFor(FontAwesomeSettings.IconKey iconKey) {
-        return FontAwesomeSettings
-            .get(Application.get())
-            .getIconType(iconKey)
-            .cssClassName();
-    }
+	private String getCssClassNameFor(FontAwesomeSettings.IconKey iconKey) {
+		return FontAwesomeSettings
+			.get(Application.get())
+			.getIconType(iconKey)
+			.cssClassName();
+	}
 
-    public RatingConfig withStart(Integer value) {
-        put(start, value);
-        return this;
-    }
+	public RatingConfig withStart(Integer value) {
+		put(start, value);
+		return this;
+	}
 
-    public RatingConfig withStop(Integer value) {
-        put(stop, value);
-        return this;
-    }
+	public RatingConfig withStop(Integer value) {
+		put(stop, value);
+		return this;
+	}
 
-    public RatingConfig withStep(Integer value) {
-        put(step, value);
-        return this;
-    }
+	public RatingConfig withStep(Integer value) {
+		put(step, value);
+		return this;
+	}
 
-    public RatingConfig withFractions(Integer value) {
-        put(fractions, value);
-        return this;
-    }
+	public RatingConfig withFractions(Integer value) {
+		put(fractions, value);
+		return this;
+	}
 
-    public RatingConfig withFilled(String value) {
-        put(filled, value);
-        return this;
-    }
+	public RatingConfig withFilled(String value) {
+		put(filled, value);
+		return this;
+	}
 
-    public RatingConfig withEmpty(String value) {
-        put(empty, value);
-        return this;
-    }
+	public RatingConfig withEmpty(String value) {
+		put(empty, value);
+		return this;
+	}
 
-    public RatingConfig withFilledSelected(String value) {
-        put(filledSelected, value);
-        return this;
-    }
+	public RatingConfig withFilledSelected(String value) {
+		put(filledSelected, value);
+		return this;
+	}
 
 }
