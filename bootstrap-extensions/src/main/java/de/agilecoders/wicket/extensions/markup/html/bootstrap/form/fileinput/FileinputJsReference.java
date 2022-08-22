2f7a96588165207561759c370bd5407e703609fb
Richard Eckart de Castilho <richard.eckart@gmail.com>
2022-08-22 22:23:02 +0200

    Issue #957 - BootstrapFileInput may not detect bootstrap version
    - Add dependency on BS JS to FileInput JS resource

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/fileinput/FileinputJsReference.java
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/fileinput/FileinputJsReference.java
@@ -7,9 +7,12 @@ import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
 import org.apache.wicket.markup.html.IHeaderContributor;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.settings.IBootstrapSettings;
 import de.agilecoders.wicket.core.util.Dependencies;
 
 public final class FileinputJsReference extends JavaScriptResourceReference implements IHeaderContributor {
@@ -22,8 +25,11 @@ public final class FileinputJsReference extends JavaScriptResourceReference impl
 
     @Override
     public List<HeaderItem> getDependencies() {
+        
+        IBootstrapSettings settings = Bootstrap.getSettings(Application.get());
         return Dependencies.combine(
                 super.getDependencies(),
+                JavaScriptReferenceHeaderItem.forReference(settings.getJsResourceReference()),
                 CssHeaderItem.forReference(FileinputCssReference.INSTANCE),
                 JavaScriptHeaderItem.forReference(Application.get().getJavaScriptLibrarySettings().getJQueryReference()));
     }
