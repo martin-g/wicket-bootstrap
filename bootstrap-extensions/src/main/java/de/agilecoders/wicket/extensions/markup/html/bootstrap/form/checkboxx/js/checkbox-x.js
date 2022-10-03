e7941ea550153cf6d9e895e648671de2424c1d2f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2022-10-04 00:53:08 +0300

    Closes #946 - Upgrade CheckBoxX to 1.5.7
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/checkboxx/js/checkbox-x.js
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/checkboxx/js/checkbox-x.js
@@ -1,15 +1,39 @@
 /*!
  * @copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2021
- * @version 1.5.6
+ * @version 1.5.7
  *
  * An extended checkbox plugin for bootstrap with three states and additional styles.
  *
  * For more JQuery/Bootstrap plugins and demos visit http://plugins.krajee.com
  * For more Yii related demos visit http://demos.krajee.com
  */
-(function ($) {
+(function (factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && typeof module.exports === 'object') {
+        factory(require('jquery'));
+    } else {
+        factory(window.jQuery);
+    }
+}(function ($) {
     "use strict";
-    var CheckboxX = function (element, options) {
+
+    var $h, CheckboxX;
+
+    /**
+     * Global Helper
+     */
+    $h = {
+        CHECKED: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="cbx-icon-krajee" viewBox="0 0 16 16"><path d="M13.485 1.431a1.473 1.473 0 012.104 2.062l-7.84 9.801a1.473 1.473 0 01-2.12.04L.431 8.138a1.473 1.473 0 012.084-2.083l4.111 4.112 6.82-8.69a.486.486 0 01.04-.045z"></path></svg>',
+        CROSSED: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="cbx-icon-krajee" viewBox="0 0 16 16"><path d="M1.293 1.293a1 1 0 011.414 0L8 6.586l5.293-5.293a1 1 0 111.414 1.414L9.414 8l5.293 5.293a1 1 0 01-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 01-1.414-1.414L6.586 8 1.293 2.707a1 1 0 010-1.414z"></path></svg>',
+        INDETERMINATE: '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="cbx-icon-krajee" viewBox="0 0 16 16"><path d="M0 2a2 2 0 012-2h12a2 2 0 012 2v12a2 2 0 01-2 2H2a2 2 0 01-2-2V2z"></path></svg>'
+    };
+
+    /**
+     * CheckboxX Constructor
+     */
+    CheckboxX = function (element, options) {
         var self = this;
         self.$element = $(element);
         self.disabled = self.$element.attr('disabled') || self.$element.attr('readonly');
@@ -17,6 +41,9 @@
         self.init(options);
     };
 
+    /**
+     * CheckboxX Prototype
+     */
     CheckboxX.prototype = {
         constructor: CheckboxX,
         init: function (options) {
@@ -25,6 +52,9 @@
                 css = options.inline ? 'cbx-container' : 'cbx-container cbx-block';
             if (options.theme) {
                 css += ' cbx-' + options.theme;
+            }
+            if (!options.iconChecked) {
+                options.iconChecked = options.useCrossIcon ? $h.CROSSED : $h.CHECKED
             }
             self.options = options;
             self.clearEvents();
@@ -36,8 +66,7 @@
                 self.$container = $(document.createElement("div")).addClass(css).html(self.render());
                 self.$container.insertBefore($el).append($el);
                 $el.hide();
-            }
-            else {
+            } else {
                 self.$container.before($el).addClass(css).html(self.render()).append($el);
             }
             self.$cbx = self.$container.find('.cbx');
@@ -62,6 +91,9 @@
                 $el.on('click.checkbox', function () {
                     self.change();
                 });
+            }
+            if (options.enclosedLabel) {
+                self.$container.closest('.cbx-label').removeClass('cbx-enclosed').addClass('cbx-enclosed');
             }
         },
         initCheckbox: function () {
@@ -160,11 +192,16 @@
         },
         getIndicator: function (hasThreeState) {
             var self = this, options = self.options, icon = options.iconUnchecked, $el = self.$element, val = $el.val(),
-                isInd = $el.is(':checkbox') ? $el.prop('indeterminate') :
-                    (val !== options.valueUnchecked && (self.options.threeState || hasThreeState));
-            return '<span class="cbx-icon">' +
-                (val === options.valueChecked ? options.iconChecked : (isInd ? options.iconNull : icon)) +
-                '</span>';
+                css = options.iconUncheckedCss, isInd = $el.is(':checkbox') ? $el.prop('indeterminate') :
+                (val !== options.valueUnchecked && (options.threeState || hasThreeState));
+            if (val === options.valueChecked) {
+                icon = options.iconChecked;
+                css = options.iconCheckedCss;
+            } else if (isInd) {
+                icon = options.iconNull
+                css = options.iconNullCss;
+            }
+            return '<span class="cbx-icon' + (css ? ' ' + css : '') + '">' + icon + '</span>';
         },
         render: function () {
             var self = this, options = self.options, icon = self.getIndicator(options.allowThreeValOnInit),
@@ -201,9 +238,13 @@
         theme: '',
         threeState: true,
         inline: true,
-        iconChecked: '<i class="glyphicon glyphicon-ok"></i>',
-        iconUnchecked: ' ',
-        iconNull: '<div class="cbx-icon-null"></div>',
+        iconChecked: null,
+        iconUnchecked: '',
+        iconNull: $h.INDETERMINATE,
+        iconCheckedCss: '',
+        iconUncheckedCss: '',
+        iconNullCss: '',
+        useCrossIcon: false,
         valueChecked: '1',
         valueUnchecked: '0',
         valueNull: '',
@@ -221,4 +262,4 @@
     $(document).ready(function () {
         $('input[data-toggle="checkbox-x"]').checkboxX();
     });
-})(window.jQuery);
+}));
