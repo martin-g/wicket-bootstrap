ea537c2d2404913593d9b6c53602be7fb544da84
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2022-11-03 15:33:25 +0200

    Remove unused method. Optimize imports
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- bootstrap-samples/src/main/java/de/agilecoders/wicket/samples/WicketApplication.java
+++ bootstrap-samples/src/main/java/de/agilecoders/wicket/samples/WicketApplication.java
@@ -8,14 +8,12 @@ import de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesome6C
 import de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesomeSettings;
 import org.apache.wicket.Application;
 import org.apache.wicket.Page;
-import org.apache.wicket.ResourceBundles;
 import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.WicketRuntimeException;
+import org.apache.wicket.markup.head.filter.JavaScriptFilteredIntoFooterHeaderResponse;
 import org.apache.wicket.markup.html.IPackageResourceGuard;
 import org.apache.wicket.markup.html.SecurePackageResourceGuard;
 import org.apache.wicket.protocol.http.WebApplication;
-import org.apache.wicket.request.resource.CssResourceReference;
-import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import org.apache.wicket.request.resource.caching.FilenameWithVersionResourceCachingStrategy;
 import org.apache.wicket.request.resource.caching.NoOpResourceCachingStrategy;
 import org.apache.wicket.request.resource.caching.version.CachingResourceVersion;
@@ -26,13 +24,7 @@ import org.apache.wicket.util.io.IOUtils;
 import org.apache.wicket.util.string.Strings;
 import org.wicketstuff.annotation.scan.AnnotatedMountScanner;
 
-import com.google.javascript.jscomp.CompilationLevel;
-
 import de.agilecoders.wicket.core.Bootstrap;
-import de.agilecoders.wicket.core.markup.html.RenderJavaScriptToFooterHeaderResponseDecorator;
-import de.agilecoders.wicket.core.markup.html.bootstrap.block.prettyprint.PrettifyCssResourceReference;
-import de.agilecoders.wicket.core.markup.html.bootstrap.block.prettyprint.PrettifyJavaScriptReference;
-import de.agilecoders.wicket.core.markup.html.references.ModernizrJavaScriptReference;
 import de.agilecoders.wicket.core.request.resource.caching.version.Adler32ResourceVersion;
 import de.agilecoders.wicket.core.settings.BootstrapSettings;
 import de.agilecoders.wicket.core.settings.CookieThemeProvider;
@@ -42,13 +34,7 @@ import de.agilecoders.wicket.extensions.javascript.YuiCssCompressor;
 import de.agilecoders.wicket.extensions.markup.html.bootstrap.editor.SummernoteConfig;
 import de.agilecoders.wicket.extensions.markup.html.bootstrap.editor.SummernoteFileStorage;
 import de.agilecoders.wicket.extensions.markup.html.bootstrap.editor.SummernoteStoredImageResourceReference;
-import de.agilecoders.wicket.extensions.markup.html.bootstrap.html5player.Html5PlayerCssReference;
-import de.agilecoders.wicket.extensions.markup.html.bootstrap.html5player.Html5PlayerJavaScriptReference;
-import de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.OpenWebIconsCssReference;
-import de.agilecoders.wicket.extensions.markup.html.bootstrap.jqueryui.JQueryUIJavaScriptReference;
 import de.agilecoders.wicket.extensions.request.StaticResourceRewriteMapper;
-import de.agilecoders.wicket.samples.assets.base.ApplicationJavaScript;
-import de.agilecoders.wicket.samples.assets.base.FixBootstrapStylesCssResourceReference;
 import de.agilecoders.wicket.samples.pages.HomePage;
 import de.agilecoders.wicket.sass.BootstrapSass;
 import de.agilecoders.wicket.themes.markup.html.bootswatch.BootswatchTheme;
@@ -163,39 +149,6 @@ public class WicketApplication extends WebApplication {
 
         getHeaderResponseDecorators().add(response -> new JavaScriptFilteredIntoFooterHeaderResponse(response, "scripts"));
         getRequestCycleSettings().setRenderStrategy(RequestCycleSettings.RenderStrategy.ONE_PASS_RENDER);
-    }
-
-    /**
-     * configure all resource bundles (css and js)
-     */
-    private void configureResourceBundles() {
-        ResourceBundles bundles = getResourceBundles();
-        bundles.addJavaScriptBundle(WicketApplication.class, "core.js",
-                                    (JavaScriptResourceReference) getJavaScriptLibrarySettings().getJQueryReference(),
-                                    (JavaScriptResourceReference) getJavaScriptLibrarySettings().getWicketAjaxReference(),
-                                    ModernizrJavaScriptReference.instance()
-        );
-
-        bundles.addJavaScriptBundle(WicketApplication.class, "bootstrap.js",
-                                    (JavaScriptResourceReference) Bootstrap.getSettings().getJsResourceReference(),
-                                    (JavaScriptResourceReference) PrettifyJavaScriptReference.INSTANCE,
-                                    ApplicationJavaScript.INSTANCE
-        );
-
-        getResourceBundles().addJavaScriptBundle(WicketApplication.class, "bootstrap-extensions.js",
-                                                 JQueryUIJavaScriptReference.instance(),
-                                                 Html5PlayerJavaScriptReference.instance()
-        );
-
-        bundles.addCssBundle(WicketApplication.class, "bootstrap-extensions.css",
-                             Html5PlayerCssReference.instance(),
-                             OpenWebIconsCssReference.instance()
-        );
-
-        bundles.addCssBundle(WicketApplication.class, "application.css",
-                             (CssResourceReference) PrettifyCssResourceReference.INSTANCE,
-                             FixBootstrapStylesCssResourceReference.INSTANCE
-        );
     }
 
     /**
