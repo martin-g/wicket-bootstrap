989cb3ccc53432a98fb95ae18ef214f1a6a9fb71
Vit Rozkovec <vit@latudio.com>
2023-11-28 15:00:48 +0100

    Upgrade Boostrap checkbox to v2.0.0 (#1007)

--- bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/checkbox/bootstrapcheckbox/js/bootstrap-checkbox.js
+++ bootstrap-extensions/src/main/java/de/agilecoders/wicket/extensions/markup/html/bootstrap/form/checkbox/bootstrapcheckbox/js/bootstrap-checkbox.js
@@ -1,112 +1,104 @@
-/*!
- * Bootstrap-checkbox v1.2.8 (http://vsn4ik.github.io/bootstrap-checkbox)
- * Copyright 2013-2015 Vasily A. (https://github.com/vsn4ik)
- * Licensed under the MIT license
- */
-
-/**
- * $.inArray: friends with IE8. Use Array.prototype.indexOf in future.
- * Use this.element.hidden in future.
- * $.proxy: friends with IE8. Use Function.prototype.bind in future.
- */
-
-'use strict';
-
-(function(factory) {
-  if (typeof define == 'function' && define.amd) {
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module
     define(['jquery'], factory);
-  }
-  else if (typeof exports == 'object') {
+  } else if (typeof exports === 'object') {
     // Node/CommonJS
     module.exports = factory(require('jquery'));
-  }
-  else {
+  } else {
     // Browser globals
     factory(jQuery);
   }
-})(function($) {
-  $.create = function() {
-    return $($.map(arguments, function(tagName) {
-      return document.createElement(tagName);
-    }));
-  };
-
-  function Checkboxpicker(element, options) {
-    this.element = element;
-    this.$element = $(element);
-
-    this.options = $.extend({}, $.fn.checkboxpicker.defaults, options, this.$element.data());
-
-    if (this.$element.closest('label').length) {
-      console.warn(this.options.warningMessage);
-
-      return;
-    }
-
-    this.$group = $.create('div').addClass('btn-group');
-
-    // .btn-group-justified works with <a> elements as the <button> doesn't pick up the styles
-    this.$buttons = $.create('a', 'a').addClass('btn');
-
-    // === '': <... data-reverse>
-    var reverse = this.options.reverse || this.options.reverse === '';
-
-    this.$off = this.$buttons.eq(reverse ? 1 : 0);
-    this.$on = this.$buttons.eq(reverse ? 0 : 1);
-
-    this.init();
+})(function ($) {
+  function create() {
+    return $($.map(arguments, $.proxy(document, 'createElement')));
   }
 
-  Checkboxpicker.prototype = {
-    init: function() {
-      this.$element.addClass('hidden');
+  var Checkboxpicker =
+  /*#__PURE__*/
+  function () {
+    function Checkboxpicker(element, options) {
+      this.element = element;
+      this.$element = $(element);
+      var data = this.$element.data(); // <... data-reverse>
+
+      if (data.reverse === '') {
+        data.reverse = true;
+      } // <... data-switch-always>
+
+
+      if (data.switchAlways === '') {
+        data.switchAlways = true;
+      } // <... data-html>
+
+
+      if (data.html === '') {
+        data.html = true;
+      }
+
+      this.options = $.extend({}, $.fn.checkboxpicker.defaults, options, data);
+
+      if (this.$element.closest('label').length) {
+        console.warn(this.options.warningMessage);
+        return;
+      }
+
+      this.$group = $(document.createElement('div'));
+      this.$buttons = create('button', 'button').attr('type', 'button').attr('tabindex', -1);
+      this.$off = this.$buttons.eq(this.options.reverse ? 1 : 0);
+      this.$on = this.$buttons.eq(this.options.reverse ? 0 : 1);
+      this.init();
+    }
+
+    var _proto = Checkboxpicker.prototype;
+
+    _proto.init = function init() {
+      var fn = this.options.html ? 'html' : 'text';
+      this.element.hidden = true;
+      this.$group.addClass(this.options.baseGroupCls).addClass(this.options.groupCls);
+      this.$buttons.addClass(this.options.baseCls).addClass(this.options.cls);
 
       if (this.options.offLabel) {
-        this.$off.text(this.options.offLabel);
+        this.$off[fn](this.options.offLabel);
       }
 
       if (this.options.onLabel) {
-        this.$on.text(this.options.onLabel);
+        this.$on[fn](this.options.onLabel);
       }
 
-      if (this.options.offIconClass) {
+      if (this.options.offIconCls) {
         if (this.options.offLabel) {
           // &nbsp; -- whitespace (or wrap into span)
           this.$off.prepend('&nbsp;');
-        }
+        } // $.addClass for XSS check
 
-        // $.addClass for XSS check
-        $.create('span').addClass(this.options.offIconClass).prependTo(this.$off);
+
+        create('span').addClass(this.options.iconCls).addClass(this.options.offIconCls).prependTo(this.$off);
       }
 
-      if (this.options.onIconClass) {
+      if (this.options.onIconCls) {
         if (this.options.onLabel) {
           // &nbsp; -- whitespace (or wrap into span)
           this.$on.prepend('&nbsp;');
-        }
+        } // $.addClass for XSS check
 
-        // $.addClass for XSS check
-        $.create('span').addClass(this.options.onIconClass).prependTo(this.$on);
+
+        create('span').addClass(this.options.iconCls).addClass(this.options.onIconCls).prependTo(this.$on);
       }
 
       if (this.element.checked) {
-        this.$on.addClass('active ' + this.options.onClass);
-        this.$off.addClass(this.options.defaultClass);
-      }
-      else {
-        this.$off.addClass('active ' + this.options.offClass);
-        this.$on.addClass(this.options.defaultClass);
-      }
-
-      if (this.options.style) {
-        this.$group.addClass(this.options.style);
+        this.$on.addClass('active');
+        this.$on.addClass(this.options.onActiveCls);
+        this.$off.addClass(this.options.offCls);
+      } else {
+        this.$off.addClass('active');
+        this.$off.addClass(this.options.offActiveCls);
+        this.$on.addClass(this.options.onCls);
       }
 
       if (this.element.title) {
         this.$group.attr('title', this.element.title);
-      }
-      else {
+      } else {
         // Attribute title (offTitle, onTitle) on this.$buttons not work (native) if this.element.disabled, fine!
         if (this.options.offTitle) {
           this.$off.attr('title', this.options.offTitle);
@@ -115,99 +107,100 @@
         if (this.options.onTitle) {
           this.$on.attr('title', this.options.onTitle);
         }
-      }
+      } // Keydown event only trigger if set tabindex, fine!
 
-      // Keydown event only trigger if set tabindex, fine!
-      this.$group.on('keydown', $.proxy(this.keydown, this));
 
-      // Don't trigger if <a> element has .disabled class, fine!
-      this.$group.on('click', 'a:not(.active)', $.proxy(this.click, this));
+      this.$group.on('keydown', $.proxy(this, 'keydown')); // Don't trigger if <a> element has .disabled class, fine!
 
-      this.$element.on('change', $.proxy(this.toggle_checked, this));
-      $(this.element.labels).on('click', $.proxy(this.focus, this));
-      $(this.element.form).on('reset', $.proxy(this.reset, this));
+      this.$buttons.on('click', $.proxy(this, 'click'));
+      this.$element.on('change', $.proxy(this, 'toggleChecked'));
+      $(this.element.labels).on('click', $.proxy(this, 'focus'));
+      $(this.element.form).on('reset', $.proxy(this, 'reset'));
+      this.$group.append(this.$buttons).insertAfter(this.element); // Necessarily after this.$group.append() (autofocus)
 
-      this.$group.append(this.$buttons).insertAfter(this.element);
-
-      // Necessarily after this.$group.append() (autofocus)
-      if (this.element.disabled) {
-        this.$buttons.addClass('disabled');
-
-        if (this.options.disabledCursor) {
-          this.$group.css('cursor', this.options.disabledCursor);
-        }
-      }
-      else {
+      if (this.element.readOnly || this.element.disabled) {
+        this.$buttons.prop('disabled', true);
+      } else {
         this.$group.attr('tabindex', this.element.tabIndex);
 
         if (this.element.autofocus) {
           this.focus();
         }
       }
-    },
-    toggle_checked: function() {
+    };
+
+    _proto.toggleChecked = function toggleChecked() {
       // this.$group not focus (incorrect on form reset)
-      this.$buttons.toggleClass('active ' + this.options.defaultClass);
-      this.$off.toggleClass(this.options.offClass);
-      this.$on.toggleClass(this.options.onClass);
-    },
-    toggle_disabled: function() {
+      this.$buttons.toggleClass('active');
+      this.$off.toggleClass(this.options.offCls);
+      this.$off.toggleClass(this.options.offActiveCls);
+      this.$on.toggleClass(this.options.onCls);
+      this.$on.toggleClass(this.options.onActiveCls);
+    };
+
+    _proto.toggleDisabled = function toggleDisabled() {
       this.$buttons.toggleClass('disabled');
 
       if (this.element.disabled) {
         this.$group.attr('tabindex', this.element.tabIndex);
-        this.$group.css('cursor', '');
-      }
-      else {
+      } else {
         this.$group.removeAttr('tabindex');
-
-        if (this.options.disabledCursor) {
-          this.$group.css('cursor', this.options.disabledCursor);
-        }
       }
-    },
-    focus: function() {
+    };
+
+    _proto.focus = function focus() {
       // Original behavior
       this.$group.trigger('focus');
-    },
-    click: function() {
-      this.change(!this.element.checked);
-    },
-    change: function(value) {
+    };
+
+    _proto.click = function click(event) {
+      // Strictly event.currentTarget. Fix #19
+      var $button = $(event.currentTarget);
+
+      if (!$button.hasClass('active') || this.options.switchAlways) {
+        this.change();
+      }
+    };
+
+    _proto.change = function change() {
+      this.set(!this.element.checked);
+    };
+
+    _proto.set = function set(value) {
       // Fix #12
       this.element.checked = value;
-
       this.$element.trigger('change');
-    },
-    keydown: function(event) {
-      if ($.inArray(event.keyCode, this.options.toggleKeyCodes) != -1) {
+    };
+
+    _proto.keydown = function keydown(event) {
+      if ($.inArray(event.keyCode, this.options.toggleKeyCodes) !== -1) {
         // Off vertical scrolling on Spacebar
         event.preventDefault();
-
-        this.click();
-      }
-      else if (event.keyCode == 13) {
+        this.change();
+      } else if (event.keyCode === 13) {
         $(this.element.form).trigger('submit');
       }
-    },
-    reset: function() {
+    };
+
+    _proto.reset = function reset() {
       // this.element.checked not used (incorect on large number of form elements)
-      if ((this.element.defaultChecked && this.$off.hasClass('active')) || (!this.element.defaultChecked && this.$on.hasClass('active'))) {
-        this.change(this.element.defaultChecked);
+      if (this.element.defaultChecked && this.$off.hasClass('active') || !this.element.defaultChecked && this.$on.hasClass('active')) {
+        this.set(this.element.defaultChecked);
       }
-    }
-  };
+    };
 
-  // Be hooks friendly
-  var oldPropHooks = $.extend({}, $.propHooks);
+    return Checkboxpicker;
+  }(); // Be hooks friendly
 
-  // Support $.fn.prop setter (checked, disabled)
+
+  var oldPropHooks = $.extend({}, $.propHooks); // Support $.fn.prop setter (checked, disabled)
+
   $.extend($.propHooks, {
     checked: {
-      set: function(element, value) {
+      set: function set(element, value) {
         var data = $.data(element, 'bs.checkbox');
 
-        if (data && element.checked != value) {
+        if (data && element.checked !== value) {
           data.change(value);
         }
 
@@ -217,11 +210,11 @@
       }
     },
     disabled: {
-      set: function(element, value) {
+      set: function set(element, value) {
         var data = $.data(element, 'bs.checkbox');
 
-        if (data && element.disabled != value) {
-          data.toggle_disabled();
+        if (data && element.disabled !== value) {
+          data.toggleDisabled();
         }
 
         if (oldPropHooks.disabled && oldPropHooks.disabled.set) {
@@ -229,54 +222,51 @@
         }
       }
     }
-  });
-
-  // For AMD/Node/CommonJS used elements (optional)
+  }); // For AMD/Node/CommonJS used elements (optional)
   // http://learn.jquery.com/jquery-ui/environments/amd/
-  $.fn.checkboxpicker = function(options, elements) {
+
+  $.fn.checkboxpicker = function (options, elements) {
     var $elements;
 
     if (this instanceof $) {
       $elements = this;
-    }
-    else if (typeof options == 'string') {
+    } else if (typeof options === 'string') {
       $elements = $(options);
-    }
-    else {
+    } else {
       $elements = $(elements);
     }
 
-    return $elements.each(function() {
+    return $elements.each(function () {
       var data = $.data(this, 'bs.checkbox');
 
       if (!data) {
         data = new Checkboxpicker(this, options);
-
         $.data(this, 'bs.checkbox', data);
       }
     });
-  };
+  }; // HTML5 data-*.
+  // <input data-on-label="43"> --> $('input').data('onLabel') === '43'.
 
-  // HTML5 data-*.
-  // <input data-on-label="43"> --> $('input').data('onLabel') == '43'.
+
   $.fn.checkboxpicker.defaults = {
-    style: false,
-    defaultClass: 'btn-default',
-    disabledCursor: 'not-allowed',
-    offClass: 'btn-danger',
-    onClass: 'btn-success',
+    baseGroupCls: 'btn-group',
+    baseCls: 'btn',
+    groupCls: null,
+    cls: null,
+    offCls: 'btn-default',
+    onCls: 'btn-default',
+    offActiveCls: 'btn-danger',
+    onActiveCls: 'btn-success',
     offLabel: 'No',
     onLabel: 'Yes',
     offTitle: false,
     onTitle: false,
-
+    iconCls: 'glyphicon',
     // Event key codes:
     // 13: Return
     // 32: Spacebar
     toggleKeyCodes: [13, 32],
-
     warningMessage: 'Please do not use Bootstrap-checkbox element in label element.'
   };
-
-  return $.fn.checkboxpicker;
+  return Checkboxpicker;
 });
