67bf5ebc279ec0c6e9eff78bfe02cc35d129a7f8
Vit Rozkovec <vit@latudio.com>
2023-12-11 10:30:47 +0100

    Wicket 10.x bootstrap 5.x javadoc, enum final fields (#1022)
    * Remove unused imports, remove incorrect @author javadoc
    
    * Make enum fields final

--- bootstrap-samples/src/main/java/de/agilecoders/wicket/samples/panels/PanelTypesPanel.java
+++ bootstrap-samples/src/main/java/de/agilecoders/wicket/samples/panels/PanelTypesPanel.java
@@ -2,23 +2,12 @@ package de.agilecoders.wicket.samples.panels;
 
 import java.util.Arrays;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.image.ExternalImage;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-
-import de.agilecoders.wicket.core.markup.html.bootstrap.button.BootstrapButton;
-import de.agilecoders.wicket.core.markup.html.bootstrap.button.Buttons;
 import de.agilecoders.wicket.core.markup.html.bootstrap.panel.PanelType;
 
-/**
- * @author Jan Ferko
- */
 public class PanelTypesPanel extends Panel {
 
 	public PanelTypesPanel(String id) {
