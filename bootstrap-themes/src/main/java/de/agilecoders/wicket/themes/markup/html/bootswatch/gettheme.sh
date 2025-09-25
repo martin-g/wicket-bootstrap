c36fdd9b80dba16416bc6c2174d02d0dcae89e12
Erwan Tanajaya <r_1gm@yahoo.com>
2025-09-25 20:02:45 +0700

    Update Bootswatch theme to 5.3.8

--- bootstrap-themes/src/main/java/de/agilecoders/wicket/themes/markup/html/bootswatch/gettheme.sh
+++ bootstrap-themes/src/main/java/de/agilecoders/wicket/themes/markup/html/bootswatch/gettheme.sh
@@ -27,6 +27,6 @@ downloadTheme() {
 
 for theme in brite cerulean cosmo cyborg darkly flatly journal litera lumen lux materia minty morph pulse quartz sandstone simplex sketchy slate solar spacelab superhero united vapor yeti zephyr
 do
-    downloadTheme $theme 'v5.3.5'
+    downloadTheme $theme 'v5.3.8'
 done
 
